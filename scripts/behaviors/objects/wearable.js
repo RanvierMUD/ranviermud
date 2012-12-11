ce9ccf604899b08a22981fb096f0b48f6674df2f
Ben Sparks <bsparks@unicon.net>
2012-12-10 22:57:19 -0700

    fix helmet; better motd

--- scripts/behaviors/objects/wearable.js
+++ scripts/behaviors/objects/wearable.js
@@ -1,13 +1,13 @@
 // wearable.js
 
 exports.listeners = {
-    wear: function(I10n) {
+    wear: function(l10n) {
         return function(location, player, players) {
             player.sayL10n(l10n, 'WEAR', this.getShortDesc(player.getLocale()));
             player.equip(location, this);
         };
     },
-    remove: function(I10n) {
+    remove: function(l10n) {
         return function (player)
         {
             player.sayL10n(l10n, 'REMOVE', this.getShortDesc(player.getLocale()));
