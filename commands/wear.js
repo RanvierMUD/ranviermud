ad8aeec5036c8aab0f38b30ebeaf510c7bc41c42
Ben Sparks <bsparks@unicon.net>
2012-12-11 20:41:13 -0700

    convert spaces to tabs for compliance

--- commands/wear.js
+++ commands/wear.js
@@ -6,19 +6,19 @@ var l10n = require('../src/l10n')(l10n_file);
 
 exports.command = function (rooms, items, players, npcs, Commands)
 {
-    return function (args, player)
-    {
-        var wear = player.getEquipped('wear');
-        if (wear) {
-            player.sayL10n(l10n, 'CANT_WEAR', items.get(wear).getShortDesc(player.getLocale()));
-            return;
-        }
-        var thing = args.split(' ')[0];
-        thing = CommandUtil.findItemInInventory(thing, player, true);
-        if (!thing) {
-            player.sayL10n(l10n, 'ITEM_NOT_FOUND');
-            return;
-        }
-        thing.emit('wear', 'wear', player, players);
-    };
+	return function (args, player)
+	{
+		var wear = player.getEquipped('wear');
+		if (wear) {
+			player.sayL10n(l10n, 'CANT_WEAR', items.get(wear).getShortDesc(player.getLocale()));
+			return;
+		}
+		var thing = args.split(' ')[0];
+		thing = CommandUtil.findItemInInventory(thing, player, true);
+		if (!thing) {
+			player.sayL10n(l10n, 'ITEM_NOT_FOUND');
+			return;
+		}
+		thing.emit('wear', 'wear', player, players);
+	};
 };
