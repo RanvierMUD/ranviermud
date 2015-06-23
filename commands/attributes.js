3b2d60d9c7a0d9ab1e08f367c2b7394237ffde60
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-06-23 16:09:30 -0400

    Removes localization of attributes, for now

--- commands/attributes.js
+++ commands/attributes.js
@@ -2,7 +2,6 @@ var l10n_file = __dirname + '/../l10n/commands/inventory.yml';
 var l10n = require('../src/l10n')(l10n_file);
 exports.command = function(rooms, items, players, npcs, Commands) {
 	return function(args, player) {
-		player.sayL10n(l10n, 'ATTR');
 
 		var displayed = {};
 		displayed = player.getAttributes();
