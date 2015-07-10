d85138d16daf090e6f8edc94a65bc617aa1a7958
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-07-10 00:30:30 -0400

    Fixes crash-causing typo in NPC script.

--- scripts/npcs/2.js
+++ scripts/npcs/2.js
@@ -5,7 +5,7 @@ exports.listeners = {
 		{
 			var rand = Math.floor(Math.random() * 5 + 1);
 			if (rand === 3) {
-				player.say("The revenant turns and stares at you, its dull eyes skewed in opposite directions.");dw
+				player.say("The revenant turns and stares at you, its dull eyes skewed in opposite directions.");
 			}
 		}
 	},
