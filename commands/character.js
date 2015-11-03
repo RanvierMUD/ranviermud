1d0a5493ff513ffbdbdcc85fe37e43ce2d05f194
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-02 21:15:58 -0500

    Format strings for l10n, add l10n to character display

--- commands/character.js
+++ commands/character.js
@@ -1,18 +1,25 @@
 var l10n_file = __dirname + '/../l10n/commands/character.yml';
 var l10n = require('../src/l10n')(l10n_file);
 exports.command = function(rooms, items, players, npcs, Commands) {
-	
-	return function(args, player) {
-		var character = player.getAttributes() || {};
-		var name = player.getName() || "";
-		var hr = player.say("\n=======================");
-		console.log("Character attributes are: ", character);
-		player.sayL10n(l10n, 'NAME', name);
-		player.sayL10n(l10n, 'ATTRIBUTES');
-		hr();
-		for (attr in character) {
-			player.sayL10n(l10n, attr.toUppercase(), character[attr]);
-		}
-		hr();
-	};
+
+  function capitalizeFirstLetter(string) {
+    if (typeof string == 'string')
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    return string;
+  }
+
+  return function(args, player) {
+    var character = player.getAttributes() || {};
+    var hr = function() {
+      player.say("\n=======================");
+    };
+    hr();
+    player.say("|| " + player.getName());
+    hr();
+    player.sayL10n(l10n, 'ATTRIBUTES');
+    for (var attr in character) {
+      player.sayL10n(l10n, attr.toUpperCase(), capitalizeFirstLetter(character[attr]));
+    }
+    hr();
+  };
 };
