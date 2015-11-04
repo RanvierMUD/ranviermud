b836efa6519fde30dae269ffc14c8423f25711f2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-04 13:33:49 -0500

    Add error handling for commands without docs.

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,14 @@ var l10n_file = __dirname + '/../l10n/commands/help.yml';
 var l10n = require('../src/l10n')(l10n_file);
 exports.command = function(rooms, items, players, npcs, Commands) {
   return function(args, player) {
+
+    function commandNotFound() {
+      player.writeL10n(l10n, 'NOT_FOUND');
+      return;
+    }
+
     if (!args) {
-      player.writeL10n(l10n, 'USING_HELP');
+      player.writeL10n(l10n, 'HELP');
       return;
     }
 
@@ -13,11 +19,16 @@ exports.command = function(rooms, items, players, npcs, Commands) {
     }
 
     if (commands[args]) {
-      player.writeL10n(l10n, args.toUpperCase());
-      return;
+      try {
+        player.writeL10n(l10n, args.toUpperCase());
+        return;
+      } catch (err) {
+   		player.writeL10n(l10n, 'NO_HELP_FILE');
+   		return;
+      }
     }
-
     player.writeL10n(l10n, 'NOT_FOUND');
     return;
+
   };
 };
