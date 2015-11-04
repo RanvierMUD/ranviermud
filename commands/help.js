7ca367ebcae3840643bdab86b049603e085ad1c1
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-04 10:52:50 -0500

    Logic for help command

---
+++ commands/help.js
@@ -0,0 +1,23 @@
+var l10n_file = __dirname + '/../l10n/commands/help.yml';
+var l10n = require('../src/l10n')(l10n_file);
+exports.command = function(rooms, items, players, npcs, Commands) {
+  return function(args, player) {
+    if (!args) {
+      player.sayL10n(l10n, 'USING_HELP');
+      return;
+    }
+
+    var commands = {};
+    for (var command in Commands.player_commands) {
+      commands[command] = true;
+    }
+
+    if (commands[args]) {
+      player.sayL10n(l10n, args.toUpperCase();
+      return;
+    }
+
+    player.sayL10n(l10n, 'NOT_FOUND');
+
+  };
+};
