dcd48a2561ce6c29e1eaa4c98a894cf424bd3292
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-09 16:18:33 -0500

    Add file for say command logic

---
+++ commands/say.js
@@ -0,0 +1,10 @@
+var l10n_file = __dirname + '/../l10n/commands/save.yml';
+var l10n = require('../src/l10n')(l10n_file);
+var CommandUtil = require('../src/command_util').CommandUtil;
+exports.command = function (rooms, items, players, npcs, Commands)
+{
+	return function (args, player)
+	{
+		// code goes here
+	}
+};
