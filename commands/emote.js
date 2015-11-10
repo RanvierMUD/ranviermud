9937547f0fe85eb37446590c32fcb8937f998c52
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-10 13:57:36 -0500

    Fix weird bug with conditional. Function scope weirdness, maybe?

--- commands/emote.js
+++ commands/emote.js
@@ -7,21 +7,20 @@ exports.command = function(rooms, items, players, npcs, Commands) {
     if (args) {
       player.say(self + " " + args);
       players.eachIf(function(p) {
-        return otherPlayersInRoom(p);
+        return otherPlayersInRoom(p, self);
       }, function(p) {
         p.say(self + " " + args);
       });
       return;
     }
-
     player.sayL10n(l10n, 'NOTHING_EMOTED');
     return;
   }
 
-  function otherPlayersInRoom(p) {
+  function otherPlayersInRoom(p, self) {
     if (p)
-      return (p.getName() !== player.getName() && p.getLocation() === player.getLocation());
+      return (p.getName().toLowerCase() !== self.toLowerCase() 
+        && p.getLocation() === player.getLocation());
   };
-
 
 };
