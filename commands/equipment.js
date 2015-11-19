796457571f50c68af0f7d83196b691dd5b24a873
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-19 16:38:07 -0500

    Remove debug logs.

--- commands/equipment.js
+++ commands/equipment.js
@@ -4,11 +4,8 @@ exports.command = function (rooms, items, players, npcs, Commands)
 	return function (args, player)
 	{
 		var equipped = player.getEquipped();
-		console.log("EQ: ",equipped);
 		for (var i in equipped) {
-			console.log("i: ",i)
 			var item = items.get(equipped[i]);
-			console.log("item: ",item);
 			player.say(sprintf("%-15s %s", "<" + i + ">", item.getShortDesc(player.getLocale())));
 		}
 	};
