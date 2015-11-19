e8713d52764d03e94c0d67011c912aaaeae27980
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-19 16:33:08 -0500

    I think the logic is not awful now? Removed conditional that looked at nonexistent length property.

--- commands/equipment.js
+++ commands/equipment.js
@@ -1,12 +1,15 @@
 var sprintf = require('sprintf').sprintf;
-exports.command = function(rooms, items, players, npcs, Commands) {
-  return function(args, player) {
-    var equipped = player.getEquipped();
-    if (equipped.length) {
-      for (var i in equipped) {
-        var item = items.get(equipped[i]);
-        player.say(sprintf("%-15s %s", "<" + i + ">", item.getShortDesc(player.getLocale())));
-      }
-    }
-  }
+exports.command = function (rooms, items, players, npcs, Commands)
+{
+	return function (args, player)
+	{
+		var equipped = player.getEquipped();
+		console.log("EQ: ",equipped);
+		for (var i in equipped) {
+			console.log("i: ",i)
+			var item = items.get(equipped[i]);
+			console.log("item: ",item);
+			player.say(sprintf("%-15s %s", "<" + i + ">", item.getShortDesc(player.getLocale())));
+		}
+	};
 };
--- commands/look.js
+++ commands/look.js
@@ -2,139 +2,138 @@ var CommandUtil = require('../src/command_util').CommandUtil;
 var sprintf = require('sprintf').sprintf;
 var l10n_file = __dirname + '/../l10n/commands/look.yml';
 var l10n = new require('localize')(require('js-yaml').load(require('fs').readFileSync(l10n_file).toString('utf8')), undefined, 'zz');
-exports.command = function(rooms, items, players, npcs, Commands) {
+exports.command = function (rooms, items, players, npcs, Commands) {
 
-    return function(args, player) {
-        var room = rooms.getAt(player.getLocation());
-        var locale = player.getLocale();
+  return function (args, player) {
+    var room = rooms.getAt(player.getLocation());
+    var locale = player.getLocale();
 
-        if (args) {
-          // Look at items in the room first
-          var thing = CommandUtil.findItemInRoom(items, args, room, player, true);
+    if (args) {
+      // Look at items in the room first
+      var thing = CommandUtil.findItemInRoom(items, args, room, player, true);
 
-          if (!thing) {
-            // Then the inventory
-            thing = CommandUtil.findItemInInventory(args, player, true);
-          }
+      if (!thing) {
+        // Then the inventory
+        thing = CommandUtil.findItemInInventory(args, player, true);
+      }
 
-          if (!thing) {
-            // then for an NPC
-            thing = CommandUtil.findNpcInRoom(npcs, args, room, player, true);
-          }
+      if (!thing) {
+        // then for an NPC
+        thing = CommandUtil.findNpcInRoom(npcs, args, room, player, true);
+      }
 
-          function isLookingAtSelf() {
-            return args.toLowerCase() === 'me' ||
-              args.toLowerCase() === 'self' ||
-              args.toLowerCase() === player.getName().toLowerCase()
-          };
+      function isLookingAtSelf() {
+        return args.toLowerCase() === 'me' ||
+          args.toLowerCase() === 'self' ||
+          args.toLowerCase() === player.getName().toLowerCase()
+      };
 
-          if (!thing && isLookingAtSelf()) {
-            thing = player;
-          }
+      if (!thing && isLookingAtSelf()) {
+        thing = player;
+      }
 
-          if (!thing) {
-            players.eachIf(function(p) {
-                return otherPlayersInRoom(p);
-              },
-              function(p) {
-                if (args.toLowerCase() === p.getName().toLowerCase()) {
-                  thing = p;
-                  player.sayL10n(l10n, 'IN_ROOM', thing.getName());
-                  player.say(thing.getDescription());
-                  player.say(showPlayerEquipment(p, player));
-                  p.sayL10n(l10n, 'BEING_LOOKED_AT', player.getName());
-                }
-              });
-          }
-
-          if (!thing) {
-            // then look at exits
-            var exits = room.getExits();
-            exits.forEach(function(exit) {
-              if (args.toLowerCase() === exit.direction) {
-                thing = rooms.getAt(exit.location);
-                player.say(thing.getTitle(locale));
-              }
-            });
-          }
-
-          if (!thing) {
-            player.sayL10n(l10n, 'ITEM_NOT_FOUND');
-            return;
-          }
-
-          player.say(thing.getDescription(player.getLocale()));
-          return;
-        }
-
-
-        if (!room) {
-          player.sayL10n(l10n, 'LIMBO');
-          return;
-        }
-
-        // Render the room and its exits
-        player.say(room.getTitle(player.getLocale()));
-        player.say(room.getDescription(player.getLocale()));
-        player.say('');
-
-        // display players in the same room
-        players.eachIf(function(p) {
+      if (!thing) {
+        players.eachIf(function (p) {
             return otherPlayersInRoom(p);
           },
-          function(p) {
-            player.sayL10n(l10n, 'IN_ROOM', p.getName());
+          function (p) {
+            if (args.toLowerCase() === p.getName().toLowerCase()) {
+              thing = p;
+              player.sayL10n(l10n, 'IN_ROOM', thing.getName());
+              player.say(thing.getDescription());
+              player.say(showPlayerEquipment(p, player));
+              p.sayL10n(l10n, 'BEING_LOOKED_AT', player.getName());
+            }
           });
+      }
 
-        // show all the items in the rom
-        room.getItems().forEach(function(id) {
-          player.say('<magenta>' + items.get(id).getShortDesc(player.getLocale()) + '</magenta>');
-        });
-
-        // show all npcs in the room
-        room.getNpcs().forEach(function(id) {
-          var npc = npcs.get(id);
-          if (npc) {
-            var color = 'cyan';
-            switch (true) {
-              case ((npc.getAttribute('level') - player.getAttribute('level')) > 3):
-                color = 'red';
-                break;
-              case ((npc.getAttribute('level') - player.getAttribute('level')) >= 1):
-                color = 'yellow';
-                break;
-              default:
-                color = 'green'
-                break;
-            }
-            player.say('<' + color + '>' + npcs.get(id).getShortDesc(player.getLocale()) + '</' + color + '>');
+      if (!thing) {
+        // then look at exits
+        var exits = room.getExits();
+        exits.forEach(function (exit) {
+          if (args.toLowerCase() === exit.direction) {
+            thing = rooms.getAt(exit.location);
+            player.say(thing.getTitle(locale));
           }
         });
+      }
 
-        player.write('[');
-        player.writeL10n(l10n, 'EXITS');
-        player.write(': ');
-        room.getExits().forEach(function(exit) {
-          player.write(exit.direction + ' ');
-        });
-        player.say(']');
+      if (!thing) {
+        player.sayL10n(l10n, 'ITEM_NOT_FOUND');
+        return;
+      }
 
-        function otherPlayersInRoom(p) {
-          if (p)
-            return (p.getName() !== player.getName() && p.getLocation() === player.getLocation());
-        };
+      player.say(thing.getDescription(player.getLocale()));
+      return;
+    }
 
-        function showPlayerEquipment(playerTarget, playerLooking) {
-          var equipped = playerTarget.getEquipped();
-          if (equipped.length) {
-            for (var i in equipped) {
-              var item = items.get(equipped[i]);
-              playerLooking.say(sprintf("%-15s %s", "<" + i + ">", item.getShortDesc(playerLooking.getLocale())));
-                }
-              } else {
-                playerLooking.say('NAKED');
-              }
-            }
 
-          }
-        };
+    if (!room) {
+      player.sayL10n(l10n, 'LIMBO');
+      return;
+    }
+
+    // Render the room and its exits
+    player.say(room.getTitle(player.getLocale()));
+    player.say(room.getDescription(player.getLocale()));
+    player.say('');
+
+    // display players in the same room
+    players.eachIf(function (p) {
+        return otherPlayersInRoom(p);
+      },
+      function (p) {
+        player.sayL10n(l10n, 'IN_ROOM', p.getName());
+      });
+
+    // show all the items in the rom
+    room.getItems().forEach(function (id) {
+      player.say('<magenta>' + items.get(id).getShortDesc(player.getLocale()) + '</magenta>');
+    });
+
+    // show all npcs in the room
+    room.getNpcs().forEach(function (id) {
+      var npc = npcs.get(id);
+      if (npc) {
+        var color = 'cyan';
+        switch (true) {
+          case ((npc.getAttribute('level') - player.getAttribute('level')) > 3):
+            color = 'red';
+            break;
+          case ((npc.getAttribute('level') - player.getAttribute('level')) >= 1):
+            color = 'yellow';
+            break;
+          default:
+            color = 'green'
+            break;
+        }
+        player.say('<' + color + '>' + npcs.get(id).getShortDesc(player.getLocale()) + '</' + color + '>');
+      }
+    });
+
+    player.write('[');
+    player.writeL10n(l10n, 'EXITS');
+    player.write(': ');
+    room.getExits().forEach(function (exit) {
+      player.write(exit.direction + ' ');
+    });
+    player.say(']');
+
+    function otherPlayersInRoom(p) {
+      if (p)
+        return (p.getName() !== player.getName() && p.getLocation() === player.getLocation());
+    };
+
+    function showPlayerEquipment(playerTarget, playerLooking) {
+      var naked = true;
+      var equipped = playerTarget.getEquipped();
+      for (var i in equipped) {
+        var item = items.get(equipped[i]);
+        naked = false;
+        playerLooking.say(sprintf("%-15s %s", "<" + i + ">", item.getShortDesc(playerLooking.getLocale())));
+      }
+      if (naked) playerLooking.sayL10n(l10n, "NAKED"); 
+    }
+
+  }
+};
