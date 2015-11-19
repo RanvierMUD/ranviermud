816365923b75d3b69c05aef91a7beffe454ae09a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-19 16:05:03 -0500

    add sprintf so it works.

--- commands/look.js
+++ commands/look.js
@@ -1,4 +1,5 @@
 var CommandUtil = require('../src/command_util').CommandUtil;
+var sprintf = require('sprintf').sprintf;
 var l10n_file = __dirname + '/../l10n/commands/look.yml';
 var l10n = new require('localize')(require('js-yaml').load(require('fs').readFileSync(l10n_file).toString('utf8')), undefined, 'zz');
 exports.command = function(rooms, items, players, npcs, Commands) {
