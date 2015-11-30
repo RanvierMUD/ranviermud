89967b9f755121edc931c5713cbf460f27fb96c8
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-30 18:05:37 -0500

    Add logic for checking if other players are in room, add transaction function for giving an object to a player and removing it from other player's inventory.

--- commands/give.js
+++ commands/give.js
@@ -1,41 +1,44 @@
 var l10n_file = __dirname + '/../l10n/commands/give.yml';
 var l10n = require('../src/l10n')(l10n_file);
 var CommandUtil = require('../src/command_util').CommandUtil;
-exports.command = function (rooms, items, players, npcs, Commands)
-{
-	return function (args, player)
-	{
+exports.command = function (rooms, items, players, npcs, Commands) {
+    return function (args, player) {
+        // syntax 'give [target player] [item]'
+        if (player.isInCombat()) {
+            player.sayL10n(l10n, 'GIVE_COMBAT');
+            return;
+        }
 
-		// syntax 'give [target player] [item]'
-		// No giving stuff in combat
+        var room = rooms.getAt(player.getLocation());
 
-		if (player.isInCombat()) {
-			player.sayL10n(l10n, 'GIVE_COMBAT');
-			return;
-		}
+        args = args.split(' ');
+        var item = CommandUtil.findItemInInventory(args[1], player, true);
 
-		var room = rooms.getAt(player.getLocation());
-		args = args.split(' ');
+        if (!item) {
+            player.sayL10n(l10n, 'ITEM_NOT_FOUND');
+            return;
+        }
 
-		var item = CommandUtil.findItemInInventory(args[1], player, true);
-		if (!item) {
-			player.sayL10n(l10n, 'ITEM_NOT_FOUND');
-			return;
-		}
+        var target = players.eachIf(function (p) {
+            otherPlayersInRoom(p)
+        }, function (p) {
+            giveItemToPlayer(player, p, item)
+        });
 
-		var target = players.eachIf(
-			//Find out if players are in the same room
-			,
+        function giveItemToPlayer(playerGiving, playerReceiving, item) {
+            item = items.get(item);
 
-			//If they are, do all the stuff
-			)
+            playerGiving.sayL10n(l10n, 'ITEM_GIVEN', item.getShortDesc(player.getLocale()));
+            playerReceiving.sayL10n(l10n, 'ITEM_RECEIVED', item.getShortDesc(player.getLocale()));
 
-		item = items.get(item);
+            playerGiving.removeItem(item.getUuid());
+            item.setInventory(playerReceiving.getName());
+            playerReceiving.addItem(item);
+        }
 
-		player.sayL10n(l10n, 'ITEM_PICKUP', item.getShortDesc(player.getLocale()));
-		item.setRoom(null);
-		item.setInventory(player.getName());
-		player.addItem(item);
-		room.removeItem(item.getUuid());
-	};
+        function otherPlayersInRoom(p) {
+            if (p)
+                return (p.getName() !== player.getName() && p.getLocation() === player.getLocation());
+        };
+    };
 };
