b92d87c09cf3a0a3ecd95d5ed96e84b9fc11c884
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-11-30 20:01:39 -0500

    Now the item is actually removed, and there is a message displayed if your target is not found.

--- commands/give.js
+++ commands/give.js
@@ -9,10 +9,11 @@ exports.command = function (rooms, items, players, npcs, Commands) {
             return;
         }
 
-        var room = rooms.getAt(player.getLocation());
-
         args = args.split(' ');
         var item = CommandUtil.findItemInInventory(args[0], player, true);
+        var targetPlayer = args[1].toLowerCase();
+        var targetFound = false;
+        var room = rooms.getAt(player.getLocation());
 
         if (!item) {
             player.sayL10n(l10n, 'ITEM_NOT_FOUND');
@@ -22,29 +23,36 @@ exports.command = function (rooms, items, players, npcs, Commands) {
         console.log("original item", item);
         console.log("item get", items.get(item));
 
-        var targetPlayer = args[1].toLowerCase();
+
         players.eachIf(function (p) {
             return otherPlayersInRoom(p);
         }, function (p) {
             if (p.getName().toLowerCase() == targetPlayer) {
                 giveItemToPlayer(player, p, item);
+                targetFound = true;
             }
         });
 
+        if (!targetFound) {
+            player.sayL10n(l10n, "PLAYER_NOT_FOUND");
+            return;
+        }
+
         function giveItemToPlayer(playerGiving, playerReceiving, itemGiven) {
-        	console.log("player giving ", playerGiving);
-        	console.log("player receiving ", playerReceiving);
-        	console.log("item ", itemGiven);
+            console.log("player giving ", playerGiving);
+            console.log("player receiving ", playerReceiving);
+            console.log("item ", itemGiven);
+
             try {
                 playerGiving.sayL10n(l10n, 'ITEM_GIVEN', itemGiven.getShortDesc(playerGiving.getLocale()), playerReceiving.getName());
                 playerReceiving.sayL10n(l10n, 'ITEM_RECEIVED', itemGiven.getShortDesc(playerReceiving.getLocale()), playerGiving.getName());
             } catch (e) {
-            	console.log("ERROR WHEN GIVING AN ITEM --> ", e);
-            	playerGiving.sayL10n(l10n, 'GENERIC_ITEM_GIVEN', playerReceiving.getName());
-            	playerReceiving.sayL10n(l10n, 'GENERIC_ITEM_RECEIVED', playerGiving.getName());
+                console.log("ERROR WHEN GIVING AN ITEM --> ", e);
+                playerGiving.sayL10n(l10n, 'GENERIC_ITEM_GIVEN', playerReceiving.getName());
+                playerReceiving.sayL10n(l10n, 'GENERIC_ITEM_RECEIVED', playerGiving.getName());
             }
 
-            playerGiving.removeItem(itemGiven.getUuid());
+            playerGiving.removeItem(itemGiven);
             itemGiven.setInventory(playerReceiving.getName());
             playerReceiving.addItem(itemGiven);
         }
