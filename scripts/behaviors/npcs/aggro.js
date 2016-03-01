c5333e016522fd2a450d89205e8aa79683135858
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-01 00:15:05 -0500

    Add/remove TODOs.

--- scripts/behaviors/npcs/aggro.js
+++ scripts/behaviors/npcs/aggro.js
@@ -1,6 +1,6 @@
 var LevelUtils = require("../../../src/levels").LevelUtils;
 var initiate_combat = require("../../../src/rtcombat").initiate_combat;
-//TODO: Make more DRY since it is basically a copy/paste of much of the rtcombat behavior...    
+
 exports.listeners = {
   playerEnter: function(l10n) {
     var callback = function(success) {};
