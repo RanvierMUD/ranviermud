29056d8d85e6b199d9a8cad168f4192e52e38292
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-01 00:00:04 -0500

    Extract from aggro.js as well.

--- scripts/behaviors/npcs/rtcombat.js
+++ scripts/behaviors/npcs/rtcombat.js
@@ -2,12 +2,9 @@ var LevelUtils = require("../../../src/levels").LevelUtils;
 var initiate_combat = require("../../../src/rtcombat").initiate_combat;
 
 exports.listeners = {
-	combat: function (l10n)
-	{
-		return function (player, room, players, npcs, callback)
-		{
-			initiate_combat(l10n, this, player, room, npcs, callback);
-		}
-	}
+  combat: function(l10n) {
+    return function(player, room, players, npcs, callback) {
+      initiate_combat(l10n, this, player, room, npcs, callback);
+    }
+  }
 };
-
