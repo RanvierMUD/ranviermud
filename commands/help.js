a141eda02e413c303f8dcd277510c8f4705c5cd6
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-06 15:47:37 -0500

    More color for various commands.

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ exports.command = function(rooms, items, players, npcs, Commands) {
   return function(args, player) {
 
     var hr = function() {
-      player.say("\n=======================");
+      player.say("\n<purple>=======================</purple>");
     };
     
     hr();
