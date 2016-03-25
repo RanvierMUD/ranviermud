3c8a0af3e62a5810ef672922b1371e36f32fbdbf
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-25 03:17:06 -0400

    Extract method to set target pref and say something about it. Fix logic so that using an alias for a body part still works.

--- commands/target.js
+++ commands/target.js
@@ -5,36 +5,42 @@ exports.command = function(rooms, items, players, npcs, Commands) {
         var targets = {
             torso: ['torso', 'chest', 'body', 'arms', 'stomach', 'gut', 'belly'],
             head: ['head', 'face', 'eyes'],
-            hands: ['hand', 'hands'],
-            feet: ['foot', 'feet'],
-            legs: ['leg', 'legs']
+            hands: ['hand'],
+            feet: ['foot'],
+            legs: ['leg']
         };
 
         if (args) {
             if (args in targets) {
-                player.setPreference('target', args);
+                setTarget(args);
                 return;
             } else {
                 for (var targetable in targets) {
                     var found = targets[targetable].reduce(checkTargets, false);
                     if (found) {
-                        player.setPreference('target', targetable);
+                        setTarget(targetable);
                         return;
                     }
                 }
             }
         }
+
         player.sayL10n(l10n, 'NO_TARGET', player.getDescription());
         return;
 
         function checkTargets(previous, current, index, array) {
-          if (!previous) {
-            if (current) {
-              if (current === args.toLowerCase()) {
-                return true;
-              } else return false;
-            }
-          }
+            if (!previous) {
+                if (current) {
+                    if (current === args.toLowerCase()) {
+                        return true;
+                    } else return false;
+                }
+            } else return true;
+        }
+
+        function setTarget(target) {
+            player.setPreference('target', target);
+            player.sayL10n(l10n, "TARGET_SET", target);
         }
 
     }
