8b8d19594a5e2fb966c1c8ab7ab0411cb31bb1d2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-25 03:06:11 -0400

    Add target command file and yml translations. Add get and set preferences to player file.

---
+++ commands/target.js
@@ -0,0 +1,41 @@
+var l10n_file = __dirname + '/../l10n/commands/describe.yml';
+var l10n = require('../src/l10n')(l10n_file);
+exports.command = function(rooms, items, players, npcs, Commands) {
+    return function(args, player) {
+        var targets = {
+            torso: ['torso', 'chest', 'body', 'arms', 'stomach', 'gut', 'belly'],
+            head: ['head', 'face', 'eyes'],
+            hands: ['hand', 'hands'],
+            feet: ['foot', 'feet'],
+            legs: ['leg', 'legs']
+        };
+
+        if (args) {
+            if (args in targets) {
+                player.setPreference('target', args);
+                return;
+            } else {
+                for (var targetable in targets) {
+                    var found = target[targetable].reduce(checkTargets(previous, current, index, array));
+                    if (found) {
+                        player.setPreference('target', targetable);
+                        return;
+                    }
+                }
+            }
+        }
+        player.sayL10n(l10n, 'NO_TARGET', player.getDescription());
+        return;
+
+        function checkTargets(previous, current, index, array) {
+          if (!previous) {
+            if (current) {
+              if (current === args.toLowerCase()) {
+                return true;
+              } else return false;
+            }
+          }
+        }
+
+    }
+};
