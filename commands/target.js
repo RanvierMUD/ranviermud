f799c68b2140aa3aeb2e6ef1c3c9967434ffc5ba
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-25 03:08:49 -0400

    Fix some silly errors.

--- commands/target.js
+++ commands/target.js
@@ -1,4 +1,4 @@
-var l10n_file = __dirname + '/../l10n/commands/describe.yml';
+var l10n_file = __dirname + '/../l10n/commands/target.yml';
 var l10n = require('../src/l10n')(l10n_file);
 exports.command = function(rooms, items, players, npcs, Commands) {
     return function(args, player) {
@@ -16,7 +16,7 @@ exports.command = function(rooms, items, players, npcs, Commands) {
                 return;
             } else {
                 for (var targetable in targets) {
-                    var found = target[targetable].reduce(checkTargets(previous, current, index, array));
+                    var found = targets[targetable].reduce(checkTargets(previous, current, index, array));
                     if (found) {
                         player.setPreference('target', targetable);
                         return;
