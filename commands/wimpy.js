a25ebd516938bd77f362390c2f74d5e9e88bfb93
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-27 23:03:14 -0400

    Wimpy l10n and command work.

--- commands/wimpy.js
+++ commands/wimpy.js
@@ -1,4 +1,4 @@
-var l10n_file = __dirname + '/../l10n/commands/target.yml';
+var l10n_file = __dirname + '/../l10n/commands/wimpy.yml';
 var l10n = require('../src/l10n')(l10n_file);
 exports.command = function(rooms, items, players, npcs, Commands) {
     return function(args, player) {
@@ -7,7 +7,7 @@ exports.command = function(rooms, items, players, npcs, Commands) {
 
         if (wimpy && wimpy > 0 && wimpy < 100){
             player.setPreference('wimpy', wimpy);
-            player.sayL10n(l10n, 'WIMPY_SET');
+            player.sayL10n(l10n, 'WIMPY_SET', wimpy);
             return;
         }
 
