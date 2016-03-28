f974f38a55a3075f9a4023dcb94118aa6a78f416
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-28 14:02:53 -0400

    logging and improved strings for empty inventory/equipment.

--- commands/equipment.js
+++ commands/equipment.js
@@ -1,17 +1,32 @@
-var sprintf = require('sprintf').sprintf;
 var util = require('util');
+var sprintf = require('sprintf')
+  .sprintf;
+
 exports.command = function(rooms, items, players, npcs, Commands) {
   return function(args, player) {
+
     var equipped = player.getEquipped();
+
     for (var i in equipped) {
+
       var item = items.get(equipped[i]);
-      if (!item) { 
-      	util.log("Something doesn't exist: ", equipped[i]); 
-      	delete equipped[i];
+
+      if (!item) {
+        util.log("Something doesn't exist: ", equipped[i]);
+        delete equipped[i];
       }
 
       player.say(sprintf("%-15s %s", "<" + i + ">", item.getShortDesc(
         player.getLocale())));
     }
+
+
+
+    if (!Object.keys(equipped).length)
+      player.say("You are naked.");
+
+    util.log(player.getName() + '\'s equipment: ');
+    util.log(equipped);
+
   };
 };
