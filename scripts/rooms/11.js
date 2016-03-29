f4c9cb0e4f3959e98fbfe54546c00d8a3d17cff5
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-29 18:06:59 -0400

    Add ability to examine, including l10n err message, and example room with a thing to find.

--- scripts/rooms/11.js
+++ scripts/rooms/11.js
@@ -1,5 +1,7 @@
 var CommandUtil = require('../../src/command_util')
   .CommandUtil;
+var l10n_file = __dirname + '/../../l10n/scripts/rooms/11.js.yml';
+var l10n = require('../../src/l10n')(l10n_file);
 
 exports.listeners = {
   playerEnter: l10n => {
@@ -10,6 +12,7 @@ exports.listeners = {
     };
   },
 
+//TODO: Use cleverness stat for spot checks such as this.
   examine: l10n => {
     return (args, player, players) => {
       var poi = [
