c4f0725e80b6c936d0568131a752f96128b65c6b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-29 16:14:15 -0400

    Update rooms, key object, and commands to allow for unlocking and locking, with proper messaging.

--- src/commands.js
+++ src/commands.js
@@ -1,8 +1,10 @@
 var util = require('util'),
-  ansi = require('sty').parse,
+  ansi = require('sty')
+  .parse,
   fs = require('fs'),
 
-  CommandUtil = require('./command_util').CommandUtil;
+  CommandUtil = require('./command_util')
+  .CommandUtil;
 l10nHelper = require('./l10n');
 var rooms = null;
 var players = null;
@@ -63,13 +65,15 @@ var Commands = {
       // Load any npc files
       for (j in files) {
         var command_file = commands_dir + files[j];
-        if (!fs.statSync(command_file).isFile()) continue;
+        if (!fs.statSync(command_file)
+          .isFile()) continue;
         if (!command_file.match(/js$/)) continue;
 
         var command_name = files[j].split('.')[0];
 
-        Commands.player_commands[command_name] = require(command_file).command(
-          rooms, items, players, npcs, Commands);
+        Commands.player_commands[command_name] = require(command_file)
+          .command(
+            rooms, items, players, npcs, Commands);
       }
     });
   },
@@ -88,14 +92,15 @@ var Commands = {
       return false;
     }
 
-    var exits = room.getExits().filter(function(e) {
-      try {
-        var regex = new RegExp("^" + exit);
-      } catch (err) {
-        return false;
-      }
-      return e.direction.match(regex);
-    });
+    var exits = room.getExits()
+      .filter(function(e) {
+        try {
+          var regex = new RegExp("^" + exit);
+        } catch (err) {
+          return false;
+        }
+        return e.direction.match(regex);
+      });
 
     if (!exits.length) {
       return false;
@@ -131,10 +136,22 @@ exports.Commands.move = move;
  * @param Player player
  */
 function move(exit, player) {
-  rooms.getAt(player.getLocation()).emit('playerLeave', player, players);
+  rooms.getAt(player.getLocation())
+    .emit('playerLeave', player, players);
 
   if ('door' in exit && 'locked' in exit.door) {
-    player.sayL10n(l10n, 'LOCKED');
+    var key = exit.door.locked;
+
+    if (!CommandUtil.findItemInInventory(key, player)) {
+      player.sayL10n(l10n, 'LOCKED');
+      players.eachIf(p => CommandUtil.otherPlayerInRoom(player, p),
+        p => p.sayL10n(l10n, 'OTHER_LOCKED', player.getName()));
+      return;
+    }
+
+    player.sayL10n(l10n, 'UNLOCKED', key);
+    players.eachIf(p => CommandUtil.otherPlayerInRoom(player, p),
+      p => p.sayL10n(l10n, 'OTHER_UNLOCKED', player.getName(), key));
   }
 
   var room = rooms.getAt(exit.location);
@@ -144,18 +161,19 @@ function move(exit, player) {
   }
 
   // Send the room leave message
-  players.eachExcept(player, function(p) {
-    if (p.getLocation() === player.getLocation()) {
-      try {
-        var leaveMessage = player.getName() + exit.leave_message[p.getLocale()] ||
-          null;
-        if (leaveMessage) p.say(leaveMessage);
-        else p.sayL10n(l10n, 'LEAVE', player.getName());
-      } catch (e) {
-        p.sayL10n(l10n, 'LEAVE', player.getName());
+  players.eachExcept(player,
+    p => {
+      if (p.getLocation() === player.getLocation()) {
+        try {
+          var leaveMessage = player.getName() + exit.leave_message[p.getLocale()] ||
+            null;
+          if (leaveMessage) p.say(leaveMessage);
+          else p.sayL10n(l10n, 'LEAVE', player.getName());
+        } catch (e) {
+          p.sayL10n(l10n, 'LEAVE', player.getName());
+        }
       }
-    }
-  });
+    });
 
   players.eachExcept(player, function(p) {
     if (p.getLocation() === player.getLocation()) {
@@ -173,13 +191,14 @@ function move(exit, player) {
 
   // Trigger the playerEnter event
   // See example in scripts/npcs/1.js
-  room.getNpcs().forEach(function(id) {
-    var npc = npcs.get(id);
-    npc.emit('playerEnter', room, rooms, player, players, npc, npcs);
-  });
+  room.getNpcs()
+    .forEach(function(id) {
+      var npc = npcs.get(id);
+      npc.emit('playerEnter', room, rooms, player, players, npc, npcs);
+    });
 
   room.emit('playerEnter', player, players);
- 
+
   players.eachExcept(player, function(p) {
     if (p.getLocation() === player.getLocation()) {
       p.say(player.getName() + ' enters.')
