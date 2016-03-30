dc99b274885cb96997ab78c35bf7450c601b5c97
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-29 23:15:24 -0400

    Add examine script to dry storage room.

---
+++ scripts/rooms/8.js
@@ -0,0 +1,41 @@
+var CommandUtil = require('../../src/command_util')
+  .CommandUtil;
+var l10n_file = __dirname + '/../../l10n/scripts/rooms/8.js.yml';
+var l10n = require('../../src/l10n')(l10n_file);
+
+exports.listeners = {
+
+  //TODO: Use cleverness stat for spot checks such as this.
+  examine: l10n => {
+    return (args, player, players) => {
+      var poi = [
+        'crates',
+        'boxes',
+        'bags',
+        'food'
+      ];
+
+      if (poi.indexOf(args.toLowerCase()) > -1) {
+        findFood(player, players);
+        if (!player.explore('found food')) {
+          player.emit('experience', 100);
+        }
+      }
+    };
+  }
+
+};
+
+function getRand() {
+  return Math
+    .floor(Math
+      .random() * 5) + 5;
+}
+
+function seeDisturbance(player, players) {
+  player.setAttribute('health', player.getAttribute('health') +
+    getRand());
+  player.sayL10n(l10n, 'FOUND_FOOD');
+  players.eachIf(p => CommandUtil.otherPlayerInRoom(p),
+    p => { p.sayL10n(l10n, 'OTHER_FOUND_FOOD', player.getName()); });
+}
