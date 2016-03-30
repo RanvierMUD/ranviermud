538720dea7ef9682e82ebac1507eb242d8ab9695
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-30 13:51:02 -0400

    lol indentation is hard :rainbow:

--- commands/examine.js
+++ commands/examine.js
@@ -2,12 +2,18 @@ var l10n_file = __dirname + '/../l10n/commands/examine.yml';
 var l10n = require('../src/l10n')(l10n_file);
 var CommandUtil = require('../src/command_util')
   .CommandUtil;
-exports.command = function(rooms, items, players, npcs, Commands) {
-  return function(args, player) {
+var util = require('util');
 
+exports.command = function(rooms, items, players, npcs, Commands) {
+  return (args, player) => {
     if (args) {
+      args = args.trim().toLowerCase();
+
+      util.log(player.getName() + ' is searching for ' + args);
+      util.log('in ' + rooms.getAt(player.getLocation()).getTitle('en'));
+
       rooms.getAt(player.getLocation())
-        .emit('examine', args, player, players);
+        .emit('examine', args.trim(), player, players);
       return;
     }
 
