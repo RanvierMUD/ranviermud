efbbe16a1197bc86fa16ff55955e68ca8268b778
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-15 14:47:45 -0400

    Refactor and reorganize command_util file :hammer:

--- src/command_util.js
+++ src/command_util.js
@@ -1,127 +1,171 @@
-var util = require('util');
+'use strict';
+const util = require('util');
 
-var CommandUtil = {
-  /**
-   * Find an item in a room based on the syntax
-   *   things like: get 2.thing or look 6.thing or look thing
-   * @param string lookString
-   * @param Room   room
-   * @param Player player
-   * @param boolean hydrate Whether to return the id or a full object
-   * @return string UUID of the item
-   */
-  findItemInRoom: function(items, lookString, room, player, hydrate) {
-    hydrate = hydrate || false;
-    var thing = CommandUtil.parseDot(lookString, room.getItems(), function(
-      item) {
-      return items.get(item).hasKeyword(this.keyword, player.getLocale());
-    });
+const CommandUtil = {
 
-    return thing ? (hydrate ? items.get(thing) : thing) : false;
-  },
+  findItemInRoom: _findItemInRoom,
+  findItemInInventory: _findItemInInventory,
+  findNpcInRoom: _findNpcInRoom,
+  otherPlayerInRoom: inSameRoom,
+  inSameRoom: inSameRoom,
+  isDaytime: _isDaytime,
 
-  /**
-   * Find an npc in a room based on the syntax
-   *   things like: get 2.thing or look 6.thing or look thing
-   * @param string lookString
-   * @param Room   room
-   * @param Player player
-   * @param boolean hydrade Whether to return the id or a full object
-   * @return string UUID of the item
-   */
-  findNpcInRoom: function(npcs, lookString, room, player, hydrate) {
-    hydrate = hydrate || false;
-    var thing = CommandUtil.parseDot(lookString, room.getNpcs(), function(
-      id) {
-      return npcs.get(id).hasKeyword(this.keyword, player.getLocale());
-    });
-
-    return thing ? (hydrate ? npcs.get(thing) : thing) : false;
-  },
-
-  /**
-   * Find an item in inventory based on the syntax
-   *   things like: get 2.thing or look 6.thing or look thing
-   * @param string lookString
-   * @param object being This could be a player or NPC. Though most likely player
-   * @return string UUID of the item
-   */
-  findItemInInventory: function(lookString, being, hydrate) {
-    hydrate = hydrate || false;
-    var thing = CommandUtil.parseDot(lookString, being.getInventory(),
-      function(item) {
-        return item.hasKeyword(this.keyword, being.getLocale());
-      });
-
-    return thing ? (hydrate ? thing : thing.getUuid()) : false;
-  },
-
-  /**
-   * Parse 3.blah item notation
-   * @param string arg    The actual 3.blah string
-   * @param Array objects The array of objects to search in
-   * @param Function filterFunc Function to filter the list
-   * @return object
-   */
-  parseDot: function(arg, objects, filterFunc) {
-    var keyword = arg.split(' ')[0];
-    var multi = false;
-    var nth = null;
-    // Are they trying to get the nth item of a keyword?
-    if (/^\d+\./.test(keyword)) {
-      nth = parseInt(keyword.split('.')[0], 10);
-      keyword = keyword.split('.')[1];
-      multi = true;
-    }
-
-    var found = objects.filter(filterFunc, {
-      keyword: keyword,
-      nth: nth
-    });
-
-    if (!found.length) {
-      return false;
-    }
-
-    var item = null;
-    if (multi && !isNaN(nth) && nth && nth <= found.length) {
-      item = found[nth - 1];
-    } else {
-      item = found[0];
-    }
-
-    return item;
-  },
-
-  otherPlayerInRoom: function(player, target) {
-    if (target)
-      return (target.getName() !== player.getName() && target.getLocation() ===
-        player.getLocation());
-  },
-
-  isCoinFlip: isCoinFlip,
-  getRandomFromArr: getRandomFromArr,
-  isDaytime: isDaytime
+  //TODO: Make a randomUtils module:
+  isCoinFlip: _isCoinFlip,
+  getRandomFromArr: _getRandomFromArr,
+  parseDot: _parseDot,
 
 };
 
-function isCoinFlip() {
+
+
+/**
+ * @param Player|NPC entity
+ * @param Player     target
+ * @return boolean   True if they are in the same room, else false.
+ */
+function inSameRoom(entity, target) {
+  if (target) {
+    if (entity.getName) { // Handle players
+      let notSameName = target.getName() !== entity.getName();
+      let sameLocation = target.getLocation() === entity.getLocation();
+      return notSameName && sameLocation;
+    } else { // Handle NPCs
+      return entity.getRoom() === target.getLocation();
+    }
+  }
+}
+
+
+
+/**
+ * @return 0 or 1
+ */
+function _isCoinFlip() {
   return Math.round(Math.random());
 }
 
-function getRandomFromArr(arr) {
+
+
+/**
+ * @param Array arr
+ * @return * from arr
+ */
+function _getRandomFromArr(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-/*
- *  Returns a boolean. Day and night cycle switches every real-life hour.
- */
 
-function isDaytime() {
-  var time = new Date().getHours();
-  var daytime = time % 2;
+
+/**
+ * @return boolean isDaytime
+ */
+function _isDaytime() {
+  const time = new Date().getHours();
+  const daytime = time % 2;
 
   return !!daytime;
+}
+
+
+
+/**
+ * Find an item in a room based on the syntax
+ *   things like: get 2.thing or look 6.thing or look thing
+ * @param string lookString
+ * @param Room   room
+ * @param Player player
+ * @param boolean hydrate Whether to return the id or a full object
+ * @return string UUID of the item
+ */
+function _findItemInRoom(items, lookString, room, player, hydrate) {
+  hydrate = hydrate || false;
+  let thing = CommandUtil.parseDot(lookString, room.getItems(), function(
+    item) {
+    return items.get(item).hasKeyword(this.keyword, player.getLocale());
+  });
+
+  return thing ? (hydrate ? items.get(thing) : thing) : false;
+}
+
+
+
+/**
+ * Find an npc in a room based on the syntax
+ *   things like: get 2.thing or look 6.thing or look thing
+ * @param string lookString
+ * @param Room   room
+ * @param Player player
+ * @param boolean hydrade Whether to return the id or a full object
+ * @return string UUID of the item
+ */
+function _findNpcInRoom(npcs, lookString, room, player, hydrate) {
+  hydrate = hydrate || false;
+  let thing = CommandUtil.parseDot(lookString, room.getNpcs(), function(
+    id) {
+    return npcs.get(id).hasKeyword(this.keyword, player.getLocale());
+  });
+
+  return thing ? (hydrate ? npcs.get(thing) : thing) : false;
+}
+
+
+
+/**
+ * Find an item in inventory based on the syntax
+ *   things like: get 2.thing or look 6.thing or look thing
+ * @param string lookString
+ * @param object being This could be a player or NPC. Though most likely player
+ * @return string UUID of the item
+ */
+function _findItemInInventory(lookString, being, hydrate) {
+  hydrate = hydrate || false;
+  let thing = CommandUtil.parseDot(lookString, being.getInventory(),
+    function(item) {
+      return item.hasKeyword(this.keyword, being.getLocale());
+    });
+
+  return thing ? (hydrate ? thing : thing.getUuid()) : false;
+}
+
+
+
+/**
+ * Parse 3.blah item notation
+ * @param string arg    The actual 3.blah string
+ * @param Array objects The array of objects to search in
+ * @param Function filterFunc Function to filter the list
+ * @return object
+ */
+function _parseDot(arg, objects, filterFunc) {
+  let keyword = arg.split(' ')[0];
+  let multi = false;
+  let nth = null;
+
+  // Are they trying to get the nth item of a keyword?
+  if (/^\d+\./.test(keyword)) {
+    nth = parseInt(keyword.split('.')[0], 10);
+    keyword = keyword.split('.')[1];
+    multi = true;
+  }
+
+  let found = objects.filter(filterFunc, {
+    keyword: keyword,
+    nth: nth
+  });
+
+  if (!found.length) {
+    return false;
+  }
+
+  let item = null;
+  if (multi && !isNaN(nth) && nth && nth <= found.length) {
+    item = found[nth - 1];
+  } else {
+    item = found[0];
+  }
+
+  return item;
 }
 
 exports.CommandUtil = CommandUtil;
