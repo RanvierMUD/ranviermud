cf1a92b0f58f193f8467151ff07af70fb0ad297b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-23 21:44:45 -0400

    Changes to strings. Tightening up the give command as well as yell and tell broadcasting. Fix bug in rtcombat.

--- src/channels.js
+++ src/channels.js
@@ -4,11 +4,11 @@ exports.Channels = {
 	say: {
 		name: 'say',
 		description: 'Talk to those around you',
-		use: function (args, player, players)
+		use: (args, player, players) =>
 		{
 			args = args.replace("\033", '');
 			players.broadcastAt("<bold><cyan>" + player.getName() + "</cyan></bold> says '" + args + "'", player);
-			players.eachExcept(player, function (p) {
+			players.eachExcept(player, p => {
 				if (p.getLocation() === player.getLocation()) {
 					p.prompt();
 				}
@@ -19,24 +19,48 @@ exports.Channels = {
 	chat: {
 		name: 'chat',
 		description: 'Talk to everyone online',
-		use: function (args, player, players)
+		use: (args, player, players) =>
 		{
 			args = args.replace("\033", '');
 			players.broadcast("<bold><magenta>[chat] " + player.getName() + ": " + args + "</magenta></bold>", player);
-			players.eachExcept(player, function (p) { p.prompt(); });
+			players.eachExcept(player, p => p.prompt());
 		}
 	},
 
 	yell: {
 		name: 'yell',
 		description: 'Yell to everyone in the same area',
-		use: function (args, player, players, rooms)
+		use: (args, player, players, rooms) =>
 		{
-			var playerArea = rooms.getAt(player.getLocation()).getArea();
-			var vagueDesc = "a nearby " + getGenderNoun(player) + '\'s voice';
 			args = args.replace("\033", '').toUpperCase();
-			players.broadcastIf("<bold><red>[yelling] " + vagueDesc + ": " + args + "!</red></bold>", function(p){ return playerArea === rooms.getAt(p.getLocation()).getArea() && player !== p; });
-			players.eachExcept(player, function (p) { p.prompt(); });
+
+			var playerRoom = rooms.getAt(player.getLocation());
+			var playerArea = playerRoom.getArea();
+			var vagueDesc = "a nearby " + getGenderNoun(player) + '\'s voice';
+
+			players.broadcastIf("<bold><red>You hear " + vagueDesc + " yelling '" + args + "!'</red></bold>",
+				p => {
+					var otherPlayerRoom = rooms.getAt(p.getLocation());
+					var otherPlayerArea = otherPlayerRoom.getArea();
+
+					var sameArea = playerArea === otherPlayerArea;
+					var notSameRoom = playerRoom !== otherPlayerRoom;
+					var notSamePlayer = player !== p;
+
+					return sameArea && notSameRoom && notSamePlayer;
+				});
+
+			players.eachExcept(player, p => p.prompt());
+			players.broadcastIf("<bold><red>" + player.getName() + " yells '" + args + "!'</red></bold>",
+				p => {
+					var otherPlayerRoom = rooms.getAt(p.getLocation());
+					var otherPlayerArea = otherPlayerRoom.getArea();
+
+					var sameArea = playerArea === otherPlayerArea;
+					var sameRoom = playerRoom === otherPlayerRoom;
+					var notSamePlayer = player !== p;
+					return sameArea && sameRoom && notSamePlayer;
+				});
 			player.say("<bold><red>You yell, \""+args+"!\"</red></bold>");
 		}
 	},
@@ -44,20 +68,22 @@ exports.Channels = {
 	tell: {
 		name: 'tell',
 		description: 'Talk to a specific person',
-		use: function (args, player, players)
+		use: (args, player, players) =>
 		{
 			var nameEnd = args.indexOf(" ");
 			var target = args.substring(0,nameEnd);
 			var text = args.substring(nameEnd);
-			var exists = players.some(function(p){ return p.getName() === target; });
-			if (exists){
-				players.broadcastIf("<bold><magenta>" + player.getName() + " told you: " + text + "</magenta></bold>", function(p){return p.getName() === target; });
+			var exists = players.some(p => p.getName() === target);
+			var name = player.getName();
+			if (exists) {
+				players.broadcastIf("<bold><magenta>" + player.getName() + " told you: " + text + "</magenta></bold>",
+					p => p.getName() === target);
 				player.say("<bold><magenta>You told " + target + ": " + text + "</magenta></bold>", player);
-			}
-			else {
+			} else {
 				player.say("<bold><magenta>" + target + " is not logged in.</magenta></bold>", player);
 			}
-			players.eachIf(function(p){ return p.getName() === player || p.getName() === target; }, function (p) { p.prompt(); });
+			players.eachIf( p => p.getName() === name || p.getName() === target,
+			 p => p.prompt());
 		}
 	}
 };
