1581aaf5a27ee5c61f21cc46574a70f88c48f500
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-26 09:48:34 -0400

    Refactor commands :man_with_gua_pi_mao: to use es6 syntax and be slightly more clear.

--- commands/commands.js
+++ commands/commands.js
@@ -1,19 +1,20 @@
-var sprintf = require('sprintf').sprintf;
-exports.command = function (rooms, items, players, npcs, Commands)
-{
-	return function (args, player)
-	{
-		var commands = [];
-		var maxlength = 0;
-		for (var command in Commands.player_commands) {
+'use strict';
+const sprintf = require('sprintf').sprintf;
+exports.command = (rooms, items, players, npcs, Commands) => {
+	return (args, player) => {
+		let commands = [];
+		let maxlength = 0;
+		for (let command in Commands.player_commands) {
 			if (command.length > maxlength) maxlength = command.length;
 			commands.push(command);
 		}
 
 		commands.sort();
 
-		for (var i = 1; i < commands.length+1; i++) {
-			player[i % 5 === 0? 'say' : 'write'](sprintf('%-' + (maxlength + 1) + 's', commands[i-1]));
+		let len = commands.length + 1;
+		for (let i = 1; i < len; i++) {
+			let endOfColumn = i % 5 === 0;
+			player[endOfColumn ? 'say' : 'write'](sprintf('%-' + (maxlength + 1) + 's', commands[i-1]));
 		}
 	};
 };
