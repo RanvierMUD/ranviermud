61260004cc18314829165ea8195d8d574e249a3c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-27 10:54:16 -0400

    Refactor and properly log inventory and kill commands :shield:

--- commands/kill.js
+++ commands/kill.js
@@ -1,9 +1,13 @@
-var CommandUtil = require('../src/command_util').CommandUtil;
-var l10n_file = __dirname + '/../l10n/commands/kill.yml';
-var l10n = require('../src/l10n')(l10n_file);
-exports.command = function(rooms, items, players, npcs, Commands) {
-    return function(args, player) {
-        var npc = CommandUtil.findNpcInRoom(npcs, args, rooms.getAt(player.getLocation()), player, true);
+'use strict';
+const CommandUtil = require('../src/command_util').CommandUtil;
+const l10n_file = __dirname + '/../l10n/commands/kill.yml';
+const l10n = require('../src/l10n')(l10n_file);
+const util = require('util');
+exports.command = (rooms, items, players, npcs, Commands) => {
+    return (args, player) => {
+        const room = rooms.getAt(player.getLocation());
+        const npc = CommandUtil.findNpcInRoom(npcs, args, room, player, true);
+
         if (!npc) {
             player.sayL10n(l10n, 'TARGET_NOT_FOUND');
             return;
@@ -13,7 +17,8 @@ exports.command = function(rooms, items, players, npcs, Commands) {
             return;
         }
 
-        npc.emit('combat', player, rooms.getAt(player.getLocation()), players, npcs, rooms, cleanup);
+        util.log(player.getName() + ' is on the offensive...');
+        npc.emit('combat', player, room, players, npcs, rooms, cleanup);
 
         function cleanup(success) {
             // cleanup here...
