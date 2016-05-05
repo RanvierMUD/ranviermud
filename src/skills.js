cce56a11715c62008f7031ffb0280b061658e2c4
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-05-05 13:44:18 -0400

    Ran todo and stubbed out skills.js :book:

--- src/skills.js
+++ src/skills.js
@@ -1,4 +1,5 @@
 var Effects = require('./effects.js').Effects;
+var util = require('util');
 
 var l10n_dir = __dirname + '/../l10n/skills/';
 var l10ncache = {};
@@ -7,7 +8,6 @@ var l10ncache = {};
  * @return string
  */
 
-//FIXME: This entire skill system, lulz.
 
 var L = function(locale, cls, key /*, args... */ ) {
   var l10n_file = l10n_dir + cls + '.yml';
@@ -15,79 +15,34 @@ var L = function(locale, cls, key /*, args... */ ) {
   l10n.setLocale(locale);
   return l10n.translate.apply(null, [].slice.call(arguments).slice(2));
 };
+
 exports.Skills = {
-  mental: {
-    stun: {
-      type: 'active',
-      cost: 1,
-      // no prereqs
-      name: "Stun",
-      description: "Stun your opponent for physical damage. Target's attacks are slower for 5 seconds following the attack.",
-      cooldown: 4,
-      activate: function(player, args, rooms, npcs) {
-        if (!player.isInCombat()) {
-          player.say(L(player.getLocale(), 'warrior', 'TACKLE_NOCOMBAT'));
-          return true;
-        }
-
-        if (player.getEffects('cooldown_tackle')) {
-          player.say(L(player.getLocale(), 'warrior', 'TACKLE_COOLDOWN'));
-          return true;
-        }
-
-        var target = player.isInCombat();
-        if (!target) {
-          player.say("Somehow you're in combat with a ghost");
-          return true;
-        }
-
-        var damage = Math.min(target.getAttribute('max_health'), Math.ceil(player.getDamage().max * 1.2));
-
-        player.say(L(player.getLocale(), 'warrior', 'TACKLE_DAMAGE', damage));
-        target.setAttribute('health', target.getAttribute('health') - damage);
-
-        if (!target.getEffects('slow')) {
-          target.addAffect('slow', Effects.slow({
-            duration: 3,
-            magnitude: 1.5,
-            player: player,
-            target: target,
-            deactivate: function() {
-              player.say(L(player.getLocale(), 'warrior', 'TACKLE_RECOVER'));
-            }
-          }));
-        }
-
-        // Slap a cooldown on the player
-        player.addAffect('cooldown_tackle', {
-          duration: 4,
-          deactivate: function() {
-            player.say(L(player.getLocale(), 'warrior', 'TACKLE_COOLDOWN_END'));
-          }
-        });
-
-        return true;
-      }
-    }
-  },
-  physical: {
-    leatherskin: {
-      type: 'passive',
-      cost: 2,
-      // no prereqs
-      name: "Leatherskin",
-      description: "Your skin has become tougher, and you are better able to take physical damage.",
-      activate: function(player) {
-        if (player.getEffects('leatherskin')) {
-          player.removeEffect('leatherskin');
-        }
-        player.addEffect('leatherskin', Effects.health_boost({
-          magnitude: 50,
-          //consider adding +1 to stamina
-          player: player,
-          event: 'quit'
-        }));
-      }
-    }
+  //// Cleverness-related skills.
+  pick: {
+    cost: 2,
+    name: "Lockpick",
+    description: "Your ability to pick or force open locked doors or containers.",
+    attribute: "cleverness",
+    activate: () => { util.log("Activated lockpick."); };
   }
 };
+
+// physical: {
+//   leatherskin: {
+//     type: 'passive',
+//     cost: 2,
+//     // no prereqs
+//     name: "Leatherskin",
+//     description: "Your skin has become tougher, and you are better able to take physical damage.",
+//     activate: function(player) {
+//       if (player.getEffects('leatherskin')) {
+//         player.removeEffect('leatherskin');
+//       }
+//       player.addEffect('leatherskin', Effects.health_boost({
+//         magnitude: 50,
+//         //consider adding +1 to stamina
+//         player: player,
+//         event: 'quit'
+//       }));
+//     }
+//   }
