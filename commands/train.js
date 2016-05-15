d2ff66bf4b7472f7a892263a08e49143e9c6dc3e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-05-15 00:32:11 -0400

    You can now set which skill you want to train. Training it will save the information when you log out and will update your training status on login without error (probably).

--- commands/train.js
+++ commands/train.js
@@ -9,25 +9,69 @@ exports.command = (rooms, items, players, npcs, Commands) => {
       return;
     }
 
-    let targetSkill = args.slice(' ')[0].toLowerCase();
+    let targetSkill = args.split(' ')[0].toLowerCase();
 
-    for (let skill of Skills) {
-      if (skill.name.toLowerCase() === targetSkill) {
+    // Check for train none or train clear. Gets back training time.
+
+    for (let skill in Skills) {
+      const skillName = Skills[skill].name.toLowerCase();
+      util.log(targetSkill);
+      if (skillName === targetSkill) {
         util.log(player.getName() + ' is training ' + targetSkill);
-        return addSkillToQueue(skill, player);
+        return addSkillToQueue(Skills[skill], player);
       }
     }
 
-    player.say("Which skill would you like to train next?");
-    return false;
-
+    player.say("You don't have that skill.");
+    player.say("Which of your skills would you like to train?");
+    for (let skill in Skills) {
+      player.write('    ');
+      player.say('<yellow>' + Skills[skill].name + '</yellow>');
+    }
+    return;
   }
 }
 
 function displayTrainingQueue(player) {
-  player.say(player.getTraining());
+  const training = player.getTraining();
+  for (let key in training){
+    player.say (key + ': ' + training[key]);
+  }
 }
 
 function addSkillToQueue(skill, player) {
-  // DO STUFF
+  const training = player.getTraining();
+  const skills = player.getSkills();
+
+  if (training.time > skills[skill.id]) {
+    const cost = skills[skill.id] + 1;
+
+    // Each level of the skill to be trained takes 1 hour of offline 'rest' time.
+    const sessionLength = cost * 60 * 60 * 1000;
+
+    const defaultMessage =
+      '<blue>You have finished training ' +
+      skill.name +
+      '.</blue>';
+
+    const trainingSession = {
+      skill: skill.name,
+      id: skill.id,
+      duration: sessionLength,
+      newLevel: cost,
+      message: skill.trainingMessage || defaultMessage,
+    };
+
+    player.setTraining('time', training.time - cost);
+    player.setTraining(skill.id, trainingSession);
+    player.say(
+      'On resting, you will train ' +
+      skill.name.toLowerCase() +
+      ' for ' + cost + ' hours.'
+    );
+
+  } else {
+    player.say('You feel like you hit a wall when training ' + skill.name.toLowerCase() + '.');
+    player.say('You will need more time to dedicate to training to make any real progress.');
+  }
 }
