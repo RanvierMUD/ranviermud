f7103b17bc6ea03114214c7288170ca9b40137d0
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-08 15:36:38 -0400

    Basic, cursory rest command for convenience only right now.

---
+++ commands/rest.js
@@ -0,0 +1,14 @@
+'use strict';
+const l10nFile = __dirname + '/../l10n/commands/emote.yml';
+const l10n = require('../src/l10n')(l10nFile);
+const CommandUtil = require('../src/command_util').CommandUtil;
+const util = require('util');
+
+exports.command = (rooms, items, players, npcs, Commands) => {
+  return (args, player) => {
+    const self = player.getName();
+    util.log(self + ' is resting.');
+    player.say('You rest and regain health.');
+    player.emit('regen');
+  };
+};
