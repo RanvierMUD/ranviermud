3f8a4fdd9a461a4a0bfb316a8a07838124c70bca
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-11 23:40:30 -0400

    Ugh

--- scripts/behaviors/objects/wieldable.js
+++ scripts/behaviors/objects/wieldable.js
@@ -1,28 +1,28 @@
 exports.listeners = {
 	wield: function (l10n) {
-		return (location, player, players) => {
+		return function (location, player, players) {
 			location = location || body;
 			player.sayL10n(l10n, 'WIELD', this.getShortDesc(player.getLocale()));
 			player.equip(location, this);
 		}
 	},
 	remove: function (l10n) {
-		return (player) => {
+		return function (player) {
 			player.sayL10n(l10n, 'REMOVE', this.getShortDesc(player.getLocale()));
 		}
 	},
 	hit: function (l10n) {
-		return (player) => {
+		return function (player) {
 			player.sayL10n(l10n, 'HIT', this.getShortDesc(player.getLocale()));
 		}
 	},
 	miss: function (l10n) {
-		return (player) => {
+		return function (player) {
 			player.sayL10n(l10n, 'MISS');
 		}
 	},
 	parry: function (l10n) {
-		return (player) => {
+		return function (player) {
 			player.sayL10n(l10n, 'PARRY');
 		}
 	},
