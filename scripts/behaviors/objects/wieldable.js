436d31b72a1541ac5cc13a47d480c49a0e2ec18b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-11 23:39:26 -0400

    DAMN YOU, ARROW FUNCTIONS

--- scripts/behaviors/objects/wieldable.js
+++ scripts/behaviors/objects/wieldable.js
@@ -1,29 +1,27 @@
 exports.listeners = {
-	wield: (l10n) => {
+	wield: function (l10n) {
 		return (location, player, players) => {
 			location = location || body;
 			player.sayL10n(l10n, 'WIELD', this.getShortDesc(player.getLocale()));
 			player.equip(location, this);
-			//TODO: broadcast to other players in room
 		}
 	},
-	remove: (l10n) => {
+	remove: function (l10n) {
 		return (player) => {
 			player.sayL10n(l10n, 'REMOVE', this.getShortDesc(player.getLocale()));
 		}
 	},
-	hit: (l10n) => {
+	hit: function (l10n) {
 		return (player) => {
 			player.sayL10n(l10n, 'HIT', this.getShortDesc(player.getLocale()));
 		}
 	},
-	miss: (l10n) =>
-	{
+	miss: function (l10n) {
 		return (player) => {
 			player.sayL10n(l10n, 'MISS');
 		}
 	},
-	parry: (l10n) => {
+	parry: function (l10n) {
 		return (player) => {
 			player.sayL10n(l10n, 'PARRY');
 		}
