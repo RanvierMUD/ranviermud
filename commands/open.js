fc77def5ca000c93d5adb65f04bf8859acac4e83
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-13 17:09:55 -0400

    Clean up the function calls and assignments a bit.

--- commands/open.js
+++ commands/open.js
@@ -16,8 +16,7 @@ exports.command = (rooms, items, players, npcs, Commands) => {
     args = args.toLowerCase().split(' ');
 
     const room = rooms.getAt(player.getLocation());
-    const exits = room
-      .getExits()
+    const exits = room.getExits()
       .filter(exit => exit.direction.indexOf(args[0]) > -1);
 
     if (exit.length !== 1) {
@@ -42,10 +41,12 @@ exports.command = (rooms, items, players, npcs, Commands) => {
     exit.door.open = true;
 
     const dest = rooms.getAt(exit.location);
-    const otherSideDoor = dest
+    const src = rooms.getAt(player.getLocation());
+    dest
       .getExits()
       .filter(exit => exit.location === player.getLocation())
       .forEach(exit => exit.door.open = true);
+
 
     players.eachIf(
       p => CommandUtil.inSameRoom(p, player),
@@ -57,7 +58,7 @@ exports.command = (rooms, items, players, npcs, Commands) => {
     players.eachIf(
       p => p.getLocation() === exit.location,
       p => {
-        const src = rooms.getAt(player.getLocation()).getTitle('en');
+        const srcTitle = src.getTitle('en');
         p.say('The door to ' + src + ' swings open.');
       });
   };
