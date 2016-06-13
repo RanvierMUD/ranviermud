48dbe6f8aaf5ac18330cd145d83e3f1dd78aa3a4
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-13 16:50:25 -0400

    Add full close script, add broadcasting to dest room.

---
+++ commands/close.js
@@ -0,0 +1,59 @@
+'use strict';
+const CommandUtil = require('../src/command_util')
+  .CommandUtil;
+const l10nFile = __dirname + '/../l10n/commands/flee.yml';
+const util = require('util');
+const l10n = require('../src/l10n')(l10nFile);
+
+//TODO: Dry up this and open.js since they are almost the same.
+exports.command = (rooms, items, players, npcs, Commands) => {
+  return (args, player) => {
+
+    if (player.isInCombat()) {
+      player.say('You are too busy for that right now.');
+      return;
+    }
+
+    args = args.toLowerCase().split(' ');
+
+    const room = rooms.getAt(player.getLocation());
+    const exits = room
+      .getExits()
+      .filter(exit => exit.direction.indexOf(args[0]) > -1);
+
+    if (exit.length !== 1) {
+      player.say('Which door do you want to close?');
+      return;
+    }
+
+    const exit = exits[0];
+
+    if (!exit.door) {
+      player.say('There is no door.');
+      return;
+    }
+
+
+    if (exit.door.open !== true) {
+      player.say('That door is already closed.');
+      return;
+    }
+
+    player.say('You close the door.');
+    exit.door.open = false;
+    
+    players.eachIf(
+      p => CommandUtil.inSameRoom(p, player),
+      p => {
+        const dest = rooms.getAt(exit.location).getTitle('en');
+        p.say(player.getName() + ' closes the door to ' + dest + '.');
+      });
+
+    players.eachIf(
+      p => p.getLocation() === exit.location,
+      p => {
+        const src = rooms.getAt(player.getLocation()).getTitle('en');
+        p.say('The door to ' + src + ' slams shut.');
+      });
+  };
+};
