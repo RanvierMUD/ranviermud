05b848c6530de39a76a87ea11e4d9fb42f5b22f9
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-14 12:25:00 -0400

    Add meditate command and revise rest command file.

--- commands/rest.js
+++ commands/rest.js
@@ -1,6 +1,4 @@
 'use strict';
-const l10nFile = __dirname + '/../l10n/commands/emote.yml';
-const l10n = require('../src/l10n')(l10nFile);
 const CommandUtil = require('../src/command_util').CommandUtil;
 const util = require('util');
 
