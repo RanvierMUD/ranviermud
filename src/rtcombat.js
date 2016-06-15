88da793be2d006e48f69ed27515f81e60480954b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-14 21:38:45 -0400

    Fix referror and wonky conditional in rtcombat

--- src/rtcombat.js
+++ src/rtcombat.js
@@ -77,10 +77,8 @@ function _initCombat(l10n, npc, player, room, npcs, players, rooms, callback) {
 
   function combatRound(attacker, defender, a, d) {
 
-    if (attacker.hasEnergy) {
-      if (!attacker.hasEnergy(2)) {
-        a.speed = () => attacker.getSpeed() * 4;
-      }
+    if (attacker.hasEnergy && !attacker.hasEnergy(2)) {
+      a.speed = () => attacker.getAttackSpeed() * 4;
     }
 
     util.log("Speeds are " + a.speed() + ' vs. ' + d.speed());
