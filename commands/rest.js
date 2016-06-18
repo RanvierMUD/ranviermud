9510172ddd9ab643d5a9a9e931a3c74763f5a343
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-18 13:22:27 -0400

    Add check for recuperation station.

--- commands/rest.js
+++ commands/rest.js
@@ -5,10 +5,14 @@ const util = require('util');
 exports.command = (rooms, items, players, npcs, Commands) => {
   return (args, player) => {
     const self = player.getName();
+    
     if (player.isInCombat()) {
       return player.say("You can't do that while fighting.");
     }
-    
+    if (player.getEffect('recuperating')) {
+      return player.say('You are already recuperating.');
+    }
+
     util.log(self + ' is resting.');
     player.write('<blue>You rest and regain health.</blue>\n');
     player.emit('regen');
