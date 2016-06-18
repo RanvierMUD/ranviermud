1692b3e08c916e0a39e4d90b68a346578025bfed
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-18 12:29:54 -0400

    Add check for combat on rest or meditate.

--- commands/rest.js
+++ commands/rest.js
@@ -5,6 +5,10 @@ const util = require('util');
 exports.command = (rooms, items, players, npcs, Commands) => {
   return (args, player) => {
     const self = player.getName();
+    if (player.isInCombat()) {
+      return player.say("You can't do that while fighting.");
+    }
+    
     util.log(self + ' is resting.');
     player.write('<blue>You rest and regain health.</blue>\n');
     player.emit('regen');
