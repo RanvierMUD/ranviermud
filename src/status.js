61f8c152df3236d6344ef4ae3d92af5a7f5e42df
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-19 12:15:16 -0400

    Change health/sanity/energy statuses.

--- src/status.js
+++ src/status.js
@@ -14,30 +14,29 @@ function getHealthText(maxHealth, player, npc) {
     var locale = player ? player.getLocale() : 'en';
     var isPlayer = !npc;
     var percentage = getPercentage(health, maxHealth);
-    var noun = isPlayer ? getGenderNoun(player.getGender()) : 'creature';
-    var nounPhrase = isPlayer ? 'You are ' : npc.getShortDesc(locale) + ' is ';
+    var descriptor = isPlayer ? '' : npc.getShortDesc() + '\'s health: ';
     var color = getStatusColor(percentage);
 
     var healthStatus = {
-      0: 'a dead ' + noun + ' walking',
-      5: 'hanging by a thread',
-      10: 'in excruciating pain',
-      15: 'wracked by pain',
+      0: 'dead',
+      5: 'near death',
+      10: 'excruciating',
+      15: 'wrecked',
       25: 'maimed',
-      35: 'gravely wounded',
+      35: 'grave',
       50: 'wounded',
-      60: 'in awful shape',
-      70: 'feeling poor',
-      80: 'in average physical health',
-      90: 'in good physical health',
-      95: 'in great health',
-      100: 'in perfect physical health'
+      60: 'awful',
+      70: 'poor',
+      80: 'average',
+      90: 'great',
+      95: 'robust',
+      100: 'perfect',
     };
 
     for (var tier in healthStatus) {
       if (percentage <= parseInt(tier)) {
-        return '<' + color + '>' + nounPhrase + healthStatus[tier] +
-          '.</' + color + '>';
+        return '<' + color + '>' + descriptor + healthStatus[tier] +
+          '</' + color + '>';
       }
     }
   }
@@ -47,26 +46,26 @@ function getSanityText(maxSanity, player) {
   return function(sanity) {
     var percentage = getPercentage(sanity, maxSanity);
     var sanityStatus = {
-      0:  'consumed by madness',
-      10: 'nearing insanity',
-      15: 'feeling dysphoric',
-      50: 'stressed to breaking',
-      60: 'mentally unsound',
-      70: 'feeling stressed',
+      0:  'insane',
+      10: 'mad',
+      15: 'dysphoric',
+      50: 'unsound',
+      60: 'stressed',
+      70: 'ill',
       80: 'discontent',
-      90: 'mentally well',
-      95: 'quite stable',
-      100: 'sharp as a knife'
+      90: 'content',
+      95: 'stable',
+      100: 'sharp'
     };
 
     var color = getStatusColor(percentage);
     for (var tier in sanityStatus) {
       if (percentage <= parseInt(tier)) {
-        return '<' + color + '>You are ' + sanityStatus[tier] +
-          '.</' + color + '>';
+        return '<' + color + '>' + sanityStatus[tier] +
+          '</' + color + '>';
       }
     }
-    return '<' + color + '>You are feeling strange.</' + color + '>';
+    return '<' + color + '>strange</' + color + '>';
   }
 }
 
@@ -74,9 +73,9 @@ function getEnergyText(maxEnergy, player) {
   return function(energy) {
     var percentage = getPercentage(energy, maxEnergy);
     var energyStatus = {
-      0:  'unable to move',
-      10: 'dead tired',
-      15: 'exhausted',
+      0:  'depleted',
+      10: 'exhausted',
+      15: 'spent',
       50: 'drowsy',
       60: 'tired',
       70: 'fatigued',
@@ -89,11 +88,11 @@ function getEnergyText(maxEnergy, player) {
     var color = getStatusColor(percentage);
     for (var tier in energyStatus) {
       if (percentage <= parseInt(tier)) {
-        return '<' + color + '>You are ' + energyStatus[tier] +
-          '.</' + color + '>';
+        return '<' + color + '>' + energyStatus[tier] +
+          '</' + color + '>';
       }
     }
-    return '<' + color + '>You are feeling strange.</' + color + '>';
+    return '<' + color + '>strange</' + color + '>';
   }
 }
 
