052c87623ab88eb6aaa929f5d62300e2034b3f46
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-22 01:30:59 -0400

    Continue adding to helpfiles.

--- src/help_files.js
+++ src/help_files.js
@@ -8,7 +8,7 @@ const HelpFiles = {
   NEW: {
     title: 'Welcome to Ranvier',
     body: 'Important topics include:',
-    related: ['commands', 'levels', 'attributes', 'mutants', 'mental', 'physical', 'energy', 'combat', 'social'],
+    related: ['commands', 'levels', 'attributes', 'mutants', 'mental', 'physical', 'energy', 'combat', 'social', 'skills'],
   },
 
   LEVELS: {
@@ -73,49 +73,73 @@ const HelpFiles = {
   EQUIPMENT: {
     title: 'Equipment',
     body: 'List all equipped items.',
-    usage: 'equipment'
-    related: ['inventory', 'equip', 'wield', 'remove']
+    usage: 'equipment',
+    related: ['inventory', 'equip', 'wield', 'remove'],
   },
 
   KILL: {
     title: 'Kill',
     body: 'Attempt to slay a creature or character.',
     usage: ['kill (NPC name)','kill n.(NPC name) to attack the nth NPC of that same name'],
-    related: ['combat', 'stance', 'target', 'wimpy']
+    related: ['combat', 'stance', 'target', 'wimpy'],
   },
 
   REMOVE: {
     title: 'Remove',
     body: 'This will uneqip an item and place it in your inventory.',
-    usage: ['remove (item name)', 'remove 2.(item name) to remove the 2nd item of that same name' ],
-    related: ['equipment', 'inventory', 'equip', 'give'],
+    usage: ['remove (item name)', 'remove n.(item name) to remove the nth item of that same name' ],
+    related: ['equipment', 'inventory', 'wear', 'give'],
   },
 
-  WEAR: { usage: 'Usage: \n  wear (item name) \nor \n  wear 2.(item name) to wear the 2nd item of that same name \n\nThis will equip an item that is in your inventory, as long as it is wearable.\nFor weapons, use \'wield\'.' },
-  LOOK: { usage: 'Usage: \n  look\n\nThis will describe your character\'s current location.\n\nUsage: \n  look (thing)\nor\n  look 2.(thing) to look at the 2nd thing of the same name \n\nThis will describe a non-player character or item in the same room as you. \nIt can also be used to look at items in your inventory.' },
-  SAVE: { usage: 'Usage: \n  save\n\nThis will save your character\'s progress. \nUsing \'quit\' will save and then close your connection.' },
-  SKILLS: { usage: 'Usage: \n  skills\n\nThis lists all skills available to your character, including a description and the cooldown time, if applicable.' },
-  TNL: { usage: 'Usage: \n  tnl\n\nThis displays a bar of the amount of experience needed to gain your next level.' },
-  EXP: { usage: 'Usage: \n  exp\n\nThis displays a bar of the amount of experience needed to gain your next level.' },
-  WHERE: { usage: 'Usage \n  where\n\nThis displays the name of your current location.' },
-  WHO: { usage: 'Usage \n  who\n\nThis displays the name of each player who is online.' },
-  WIELD: { usage: 'Usage: \n  wield (weapon)\n\n' },
-  DESCRIBE: { usage: 'Usage: \n  describe (description)\n\nThis allows you to set a new description for your character.\n\nUsage: \n  describe\n\nThis allows you to see your current description.' },
-  APPRAISE: { usage: 'Usage \n  appraise (NPC)\n\nThis allows you to see how you might match up to an NPC in a fight.' },
-  SAY: { usage: 'Usage \n  say (message)\n\nThis sends a message to all other players in the room.' },
-  WHISPER: { usage: 'Usage \n  whisper (player) (message)\n\nThis sends a message to a single player in the same room. \nOther players can see that you are whispering, but cannot hear the message.' },
-  EMOTE: { usage: 'Usage \n  emote (action)\n\nThis allows you to roleplay as your character performing the action.\nThis message will be shown to all other players in the room, so write it in third person.\n\nExample: `emote jumps with joy` would display `Helga jumps with joy`.' },
-  GIVE: { usage: 'Usage \n give (item) (player)\n\nThis gives the specified item in your inventory to another player\'s character.\nTheir character must be in the same room as you.' },
-  TELL: { usage: 'Usage \n tell (player) (message)\n\nThis sends a private message to the player as long as they are online, even if they aren\'t in the same room.' },
-  TARGET: { usage: 'Usage \n target (body part) \n\nThis lets the player decide where to aim their attack.\nExample: `target legs` to aim at your opponents\' legs, while `target` will show your current target.' },
-  WIMPY: { usage: 'Usage \n wimpy (percentage) \n\nThis lets the player decide when to flee combat. \nExample: `wimpy 50` will cause you to flee after losing half of your life.\n`wimpy` will show your current wimpiness preference.' },
-  FLEE: { usage: 'Usage \n flee \n\nIf you are fighting, this will cause you to attempt to flee.' },
-  STANCE: { usage: 'Usage \n stance (stance) \n\nOptions:\n`normal`: This has no major effect on your combat style.\n`berserk`: You do more damage at the cost of defense.\n`cautious`: You have higher defense at the cost of damage.\n`precise`: Your attacks always hit their target, and you defend \nwell against aimed strikes, at a cost to speed.' },
-  ROOMDESCS: { usage: 'Usage \n roomdesc (preference) \n\nOptions:\n`default`: Show a verbose description the first time you enter a room, or when using the `look` command. Otherwise, show the short description.\n`short`: Always show the short description unless you use the look command.\n`verbose` Always show the verbose description.' },
-  EXAMINE: { usage: 'Usage \n examine (point of interest) \n\nExamine items in your environment to learn more. Investigate your surroundings thoroughly and you may be surprised.' },
-  MANIFEST: { usage: 'Usage \n manifest (mutation) \n\nExpend a mutagen to manifest a special mutation or talent. This is permanent, so choose wisely.' },
-  MUTATIONS: { usage: 'Usage \n mutations \n\nDisplay your current mutations and any you can `manifest` now.' },
-  TRAIN: { usage: 'Usage \n train \n\nDisplay your queued training sessions.\n\nUsage:\n train (skill)\n\nQueue a training session in the skill of your choice.\nYour character will train while you are logged out.\n\nUsage:\n train clear\n\nClears all planned and ongoing training sessions.' }
+  WEAR: {
+    title: 'Wear',
+    body: 'This will equip an item that is in your inventory, as long as it is wearable.\nFor weapons, use \'wield\'.',
+    usage: ['wear (item name)', 'wear n.(item name) to wear the nth item of that same name'],
+    related: ['equipment', 'inventory', 'remove']
+  },
+
+  LOOK: {
+    title: 'Look'
+    usage: ['look', 'look (thing)', 'look n.(thing) to look at the nth thing of the same name', 'look (direction) to look into adjacent areas'],
+    body: 'Read a description of your current location, or a thing in your immediate vicinity, including your inventory.',
+    related: ['examine'],
+  },
+
+  SAVE: {
+    title: 'Save',
+    usage: 'save',
+    body: 'This will save your character\'s progress. \nUsing \'quit\' will save and then close your connection.',
+    related: ['quit', 'character']
+  },
+
+  SKILLS: {
+    title: 'Skills -- command and overview',
+    usage: 'skills',
+    body: 'This command lists all skills available to your character, including a description of how to use the skill.\nSkills include any activity that an average human would be capable of attempting.\nAll skills are available to all player characters, however training skills will increase a character\'s odds of success and the effects of using the skill.',
+    related: ['character', 'train', 'boost'],
+  },
+
+  TNL: { usage: 'tnl This displays a bar of the amount of experience needed to gain your next level.' },
+  EXP: { usage: 'exp This displays a bar of the amount of experience needed to gain your next level.' },
+  WHERE: { usage: 'where This displays the name of your current location.' },
+  WHO: { usage: 'who This displays the name of each player who is online.' },
+  WIELD: { usage: 'wield (weapon) ' },
+  DESCRIBE: { usage: 'describe (description) This allows you to set a new description for your character. Usage: \n  describe This allows you to see your current description.' },
+  APPRAISE: { usage: 'appraise (NPC) This allows you to see how you might match up to an NPC in a fight.' },
+  SAY: { usage: 'say (message) This sends a message to all other players in the room.' },
+  WHISPER: { usage: 'whisper (player) (message) This sends a message to a single player in the same room. \nOther players can see that you are whispering, but cannot hear the message.' },
+  EMOTE: { usage: 'emote (action) This allows you to roleplay as your character performing the action.\nThis message will be shown to all other players in the room, so write it in third person. Example: `emote jumps with joy` would display `Helga jumps with joy`.' },
+  GIVE: { usage: 'give (item) (player) This gives the specified item in your inventory to another player\'s character.\nTheir character must be in the same room as you.' },
+  TELL: { usage: 'tell (player) (message) This sends a private message to the player as long as they are online, even if they aren\'t in the same room.' },
+  TARGET: { usage: 'target (body part)  This lets the player decide where to aim their attack.\nExample: `target legs` to aim at your opponents\' legs, while `target` will show your current target.' },
+  WIMPY: { usage: 'wimpy (percentage)  This lets the player decide when to flee combat. \nExample: `wimpy 50` will cause you to flee after losing half of your life.\n`wimpy` will show your current wimpiness preference.' },
+  FLEE: { usage: 'flee  If you are fighting, this will cause you to attempt to flee.' },
+  STANCE: { usage: 'stance (stance)  Options:\n`normal`: This has no major effect on your combat style.\n`berserk`: You do more damage at the cost of defense.\n`cautious`: You have higher defense at the cost of damage.\n`precise`: Your attacks always hit their target, and you defend \nwell against aimed strikes, at a cost to speed.' },
+  ROOMDESCS: { usage: 'roomdesc (preference)  Options:\n`default`: Show a verbose description the first time you enter a room, or when using the `look` command. Otherwise, show the short description.\n`short`: Always show the short description unless you use the look command.\n`verbose` Always show the verbose description.' },
+  EXAMINE: { usage: 'examine (point of interest)  Examine items in your environment to learn more. Investigate your surroundings thoroughly and you may be surprised.' },
+  MANIFEST: { usage: 'manifest (mutation)  Expend a mutagen to manifest a special mutation or talent. This is permanent, so choose wisely.' },
+  MUTATIONS: { usage: 'mutations  Display your current mutations and any you can `manifest` now.' },
+  TRAIN: { usage: 'train  Display your queued training sessions. Usage:\n train (skill) Queue a training session in the skill of your choice.\nYour character will train while you are logged out. Usage:\n train clear Clears all planned and ongoing training sessions.' }
 };
 
 module.exports = { HelpFiles };
