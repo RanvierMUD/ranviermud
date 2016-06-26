70819c70242b9f1c2a28e5536d8c110cf444f5a9
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-26 14:08:09 -0400

    Test prompt and location.

--- test/player.spec.js
+++ test/player.spec.js
@@ -5,18 +5,28 @@ const Mocks  = require('./mocks/mocks');
 
 describe('New player', () => {
 
+    const testPlayer = new Player();
+
     it('Should be of the Player class', () => {
       const testPlayer = new Player();
       expect(testPlayer instanceof Player).to.be.true;
     });
 
     it('Should have default attributes', () => {
-      const testPlayer = new Player();
-
       const expected   = Mocks.defaultAttributes;
       const actual     = testPlayer.getAttributes();
 
       expect(actual).to.eql(expected);
+    });
+
+    it('Should get a prompt string.', () => {
+      expect(testPlayer.getPrompt()).to.be.a('String');
+      expect(testPlayer.getCombatPrompt()).to.be.a('String');
+    });
+
+    it('Should be able to set and get location ', () => {
+      testPlayer.setLocation(5);
+      expect(testPlayer.getLocation() === 5).to.be.true;
     });
 
 });
