45a8b34b6d0742e61c1568975b3183879ff97d9e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-26 13:49:24 -0400

    Add mock for default attr, add test for initializing attr.

--- test/mocks/mocks.js
+++ test/mocks/mocks.js
@@ -1,1 +1,25 @@
+const defaultAttributes = {
+  // Copied from
+  max_health: 100,
+  health: 90,
+  max_sanity: 100,
+  sanity: 70,
+  energy: 40,
+  max_energy: 50,
 
+  stamina: 1,
+  willpower: 1,
+  quickness: 1,
+  cleverness: 1,
+
+  level: 1,
+  experience: 0,
+  mutagens: 0,
+  attrPoints: 0,
+
+  description: 'A person.'
+};
+
+module.exports = {
+  defaultAttributes,
+};
--- test/player.spec.js
+++ test/player.spec.js
@@ -1,10 +1,22 @@
 const Player = require('../src/player.js').Player;
 const expect = require('chai').expect;
+const Mocks  = require('./mocks/mocks');
 
 
 describe('New player', () => {
+
     it('Should be of the Player class', () => {
       const testPlayer = new Player();
       expect(testPlayer instanceof Player).to.be.true;
     });
+
+    it('Should have default attributes', () => {
+      const testPlayer = new Player();
+
+      const expected   = Mocks.defaultAttributes;
+      const actual     = testPlayer.getAttributes();
+
+      expect(actual).to.eql(expected);
+    });
+
 });
