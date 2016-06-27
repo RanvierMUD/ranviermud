a25b8a3ce85814095250509b81b117e629d8d4d3
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-27 17:17:04 -0400

    Add getScore method and redux karma stuff so it is accumulated for scorekeeping.

--- src/accounts.js
+++ src/accounts.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 const Accounts = function() {
-
+  this.accounts = [];
 };
 
 const Account = function() {
@@ -9,21 +9,34 @@ const Account = function() {
   this.username   = '';
   this.characters = [];
   this.password   = null;
+  this.karma      = 0;
+  this.score = {
+    totalKarma: 0,
+  };
 
   /* Mutators */
   this.getUsername  = ()   => this.username;
   this.setUsername  = name => this.username = name;
 
-  this.getPassword = () => this.password; // Returns hash.
+  this.addCharacter = char => this.characters.push(char);
+  this.getCharacter = name => this.characters.find(
+    char => name.toLowerCase() === char.getName().toLowerCase());
+
+  this.getPassword = ()   => this.password; // Returns hash.
   this.setPassword = pass =>
-    this.password = crypto
+    this.password  = crypto
       .createHash('md5')
       .update(pass)
       .digest('hex');
 
-  this.addCharacter = char => this.characters.push(char);
-  this.getCharacter = name => this.characters.find(
-    char => name.toLowerCase() === char.getName().toLowerCase());
+  this.getKarma    = ()    => this.karma;
+  this.deductKarma = karma => this.karma -= karma;
+  this.addKarma    = karma => {
+    this.karma += karma;
+    this.score.totalKarma += karma;
+  };
+
+  this.getScore = key => key ? this.score[key] : this.score;
 
   return this;
 };
