a626a314fb9b759fac56141c43091df2e404d50e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-26 20:39:05 -0400

    Can get and set usernames.

--- src/accounts.js
+++ src/accounts.js
@@ -3,6 +3,13 @@ const Accounts = function() {
 };
 
 const Account = function() {
+
+  this.username = '';
+
+  /* Mutators */
+  this.getUsername = ()   => this.username;
+  this.setUsername = name => this.username = name;
+
   return this;
 };
 
