2d4135720593ea1523198239a9225eb6033bc186
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-26 20:35:08 -0400

    Make account a class.

--- test/src/accounts.spec.js
+++ test/src/accounts.spec.js
@@ -13,4 +13,10 @@ describe('User Account', () => {
   it('should exist', () => {
     expect(Accounts).to.be.ok;
   });
+
+  it('should be instantiated by new', () => {
+    const testAccount = new Account();
+    expect(testAccount instanceof Account).to.be.true;
+  });
+
 });
