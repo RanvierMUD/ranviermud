40f02144ca9b063ed6469d87b0fe96c0829ed3c1
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-26 23:53:26 -0400

    Can add and get characters.

--- src/accounts.js
+++ src/accounts.js
@@ -4,11 +4,16 @@ const Accounts = function() {
 
 const Account = function() {
 
-  this.username = '';
+  this.username   = '';
+  this.characters = [];
 
   /* Mutators */
-  this.getUsername = ()   => this.username;
-  this.setUsername = name => this.username = name;
+  this.getUsername  = ()   => this.username;
+  this.setUsername  = name => this.username = name;
+
+  this.addCharacter = char => this.characters.push(char);
+  this.getCharacter = name => this.characters.find(
+    char => name.toLowerCase() === char.getName().toLowerCase());
 
   return this;
 };
