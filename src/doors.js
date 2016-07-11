cb03944c4d444900bd88eca0c0b7f7a0b4de2d3a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-11 16:55:35 -0400

    Add generic function for using keys on doors that may or may not be locked, including helper funcs for locking and unlocking doors.

--- src/doors.js
+++ src/doors.js
@@ -19,9 +19,16 @@ const updateDestination = (player, dest, callback) => dest
   .filter(exit => exit.location === player.getLocation())
   .forEach(callback);
 
+
+const changeDoorLockState = isLocked => exit => exit.door.locked = isLocked;
+const lockDoor = changeDoorLockState(true);
+const unlockDoor = changeDoorLockState(false);
+
+
 exports.Doors = {
   has,       updateDestination,
-  findExit,  openOrClose
+  findExit,  openOrClose,
+  lockDoor,  unlockDoor,
 };
 
 function openOrClose(verb, args, player, players, rooms) {
@@ -81,5 +88,68 @@ function openOrClose(verb, args, player, players, rooms) {
     p => CommandUtil.inSameRoom(p, player),
     p => p.say(player.getName() + ' ' + verb + 's the door to ' + dest + '.'));
 
+}
 
-};
+function useKey(verb, args, player, players, rooms) {
+
+  const isLocking = verb === 'lock';
+
+  player.emit('action');
+
+  if (player.isInCombat()) {
+    return player.say('You are too busy for that right now.');
+  }
+
+  args = args.toLowerCase().split(' ');
+
+  if (!args) {
+    return player.say("Which door do you want to " + verb + "?");
+  }
+
+  const room  = rooms.getAt(player.getLocation());
+  const dir   = args[0];
+  const exits = findExit(room, dir);
+
+  if (exits.length !== 1) {
+    return player.say('Be more specific...');
+  }
+
+  const exit = exits[0];
+
+  if (!exit.door) {
+    return player.say('There is no door.');
+  }
+
+  if (exit.door.open === true && isLocking) {
+    player.say('You close the door.');
+    exit.door.open = false;
+  }
+
+  if (exit.door.locked === isLocking) {
+    return player.say('That door is already ' + verb + 'ed.');
+  }
+
+  const key = CommandUtil.findItemInInventory(exit.door.key, player, true);
+
+  if (!key) {
+    return player.say('You are missing the key.');
+  }
+
+  const dest       = rooms.getAt(exit.location);
+  const srcTitle   = room.getTitle('en');
+  const destTitle  = dest.getTitle('en');
+
+  player.say('You ' + verb + ' the door.');
+  exit.door.locked = isLocking;
+
+  const lockOrUnlock = isLocking ? lockDoor : unlockDoor;
+  updateDestination(player, dest, lockOrUnlock);
+
+  players.eachIf(
+    p => CommandUtil.inSameRoom(p, player),
+    p => p.say(player.getName() + ' pulls out a key and ' + verb + 's the door to ' + destTitle + '.'));
+
+  players.eachIf(
+    p => p.getLocation() === exit.location,
+    p => p.say('You hear a click from the direction of ' + srcTitle + '.'));
+}
