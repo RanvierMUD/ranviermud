f3b5d3433bcae30fe1220413efff75791534559a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-11 02:05:30 -0400

    Clean up code and debug weird broadcasting.

--- commands/open.js
+++ commands/open.js
@@ -1,13 +1,9 @@
 'use strict';
-const CommandUtil = require('../src/command_util')
-  .CommandUtil;
-const l10nFile = __dirname + '/../l10n/commands/flee.yml';
 const util = require('util');
-const l10n = require('../src/l10n')(l10nFile);
 
+const CommandUtil = require('../src/command_util').CommandUtil;
 const Doors = require('../src/doors').Doors;
 
 
 exports.command = (rooms, items, players, npcs, Commands) =>
-  (args, player) =>
-    Doors.openOrClose('open', args, player, players, rooms);
+  (args, player) => Doors.openOrClose('open', args, player, players, rooms);
