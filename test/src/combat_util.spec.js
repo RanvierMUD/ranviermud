ce8da26869668786b48542a01e6fd4b77923e1aa
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-15 08:29:42 -0400

    Create test file and script file for combat utility funcs.

---
+++ test/src/combat_util.spec.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect;
+
+const Player = require('../../src/player.js').Player;
+const Npc    = require('../../src/npcs.js').Npc;
+
+const CombatUtil = require('../../src/combat_uti.js').CombatUtil;
