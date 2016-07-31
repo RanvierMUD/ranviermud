eb35df3362d4f635f10719fdb672ee98d7da067c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-31 09:27:52 -0400

    Fix playerEntry and playerDropItem scripts for roach.

--- scripts/npcs/1.js
+++ scripts/npcs/1.js
@@ -9,14 +9,18 @@ exports.listeners = {
     return (room, rooms, player, players, npc) => {
       const rand = Math.floor(Math.random() * 5 + 1);
       if (rand === 3) {
-        player.sayL10n(l10n, 'PLAYER_ENTER');
+        player.say('<bold>The roach waggles its antennae.</bold>');
       }
     }
   },
 
   playerDropItem: l10n  => {
-    return (room, player) => {
-      player.sayL10n(l10n, 'PLAYER_DROP');
+    return (room, player, players, item) => {
+      const rand = Math.floor(Math.random() * 5 + 1);
+      if (rand === 3) {
+        const itemDesc = item.getShortDesc();
+        player.say('<bold>The roach scurries over to the ' + itemDesc +'</bold>');
+      }
     }
   },
 
