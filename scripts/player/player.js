388c8af15b3e853ad0bd98b1527c1dd9c6142c0c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-30 20:09:59 -0400

    Attrpoints was not defined. Add new line also.

--- scripts/player/player.js
+++ scripts/player/player.js
@@ -131,8 +131,8 @@ exports.listeners = {
         this.say('<blue>You have gained ' + attr + '.</blue>');
       }
 
-      if (gainedMutation) { this.say('<blue>You may be able to `manifest` new mutations.</blue>'); }
-      if (attrPoints) { this.say('<blue>You may boost your stamina, quickness, cleverness, or willpower.</blue>'); }
+      if (gainedMutation) { this.say('\n<blue>You may be able to `manifest` new mutations.</blue>'); }
+      this.say('<blue>You may boost your stamina, quickness, cleverness, or willpower.</blue>');
 
       // Add points for skills
       const skillGain = LevelUtil.getTrainingTime(newLevel);
