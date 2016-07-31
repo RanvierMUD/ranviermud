d73d3e854fd78e29fea503f915a9ea6f4154319f
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-31 09:05:33 -0400

    Fix up use of players.eachIF to account for location.

--- src/broadcast.js
+++ src/broadcast.js
@@ -2,25 +2,26 @@ const Type = require('./type').Type;
 const util = require('util');
 
 const noop = function() {}
-const toRoom = (location, firstParty, secondParty, players) => config => {
+const toRoom = (room, firstParty, secondParty, players) => config => {
 
-  util.log('broadcast ->>>>', config);
   const firstPartyMsger = Type.isPlayer(firstParty) ?
     firstParty.say : noop;
   const secondPartyMsger = Type.isPlayer(secondParty) ?
     secondParty.say : noop;
+
+
+  const isThirdPartyInRoom = player => {
+    const isSpecificParty = party =>
+      Type.isPlayer(party) && party.getAccountName() === player.getAccountName();
+    const isFirstParty  = isSpecificParty(firstParty);
+    const isSecondParty = isSpecificParty(secondParty);
+    const inSameRoom    = room.getLocation() === player.getLocation();
+    return !isFirstParty && !isSecondParty && inSameRoom;
+  };
+
   const thirdPartyMsger = msg => players.eachIf(
-    player => {
-      const isSpecificParty = party =>
-        Type.isPlayer(party) && party.getAccountName() === player.getAccountName();
-      const isFirstParty = isSpecificParty(firstParty);
-      const isSecondParty = isSpecificParty(secondParty);
-      if (!isFirstParty && !isSecondParty) {
-        player.say(msg);
-      }
-    });
-
-
+    isThirdPartyInRoom,
+    player => player.say(msg));
 
   if (config.firstPartyMessage) {
     firstPartyMsger(config.firstPartyMessage);
