9c872e518f396e3e6b45d3dd3cce07923625b218
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-31 02:54:38 -0400

    Improve semi-random broadcasting using helper and config objects.

--- src/broadcast.js
+++ src/broadcast.js
@@ -1,11 +1,16 @@
 const noop = function() {}
-const broadcastToRoom = (location, firstParty, secondParty, players) => {
+const toRoom = (location, firstParty, secondParty, players) => config => {
 
   const firstPartyMsger = Type.isPlayer(firstParty) ?
     firstParty.say : noop;
   const secondPartyMsger = Type.isPlayer(secondParty) ?
     secondParty.say : noop;
-  const thirdPartyMsger = msg => players.eachAt(firstParty.getLocation(), player => player.say(msg)); 
-}
+  const thirdPartyMsger = msg => players.eachAt(firstParty.getLocation(), player => player.say(msg));
 
-exports.Broadcast = {};
+  firstPartyMsger(config.firstPartyMessage);
+  secondPartyMsger(config.secondPartyMessage);
+  thirdPartyMsger(config.thirdPartyMessage);
+
+};
+
+exports.Broadcast = { toRoom };
