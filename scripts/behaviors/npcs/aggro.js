7bfcfefc300f8ecc5ab402841db91d283eaca8d0
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-31 11:04:01 -0400

    Improve aggro script so that on tick or item drop, npc will attack if aggro.

--- scripts/behaviors/npcs/aggro.js
+++ scripts/behaviors/npcs/aggro.js
@@ -12,5 +12,31 @@ exports.listeners = {
         initCombat(l10n, this, player, room, npcs, players, rooms, callback);
       }
     }
-  }
+  },
+
+  playerDropItem: (l10n) => {
+    let callback = success => { /* Do stuff here*/ };
+    return function (room, rooms, player, players, npc, npcs) {
+      if (!player.isInCombat() && !npc.isInCombat()) {
+        util.log(npc.getShortDesc('en') + ' is on the offensive.');
+        initCombat(l10n, this, player, room, npcs, players, rooms, callback);
+      }
+    }
+  },
+
+  tick: (l10n) => {
+    let callback = success => { /* Do stuff here*/ };
+    return function (room, rooms, player, players, npc, npcs) {
+      players.eachIf(
+        p => p.getLocation() === npc.getLocation()
+        p => {
+          if (!p.isInCombat() && !npc.isInCombat()) {
+            util.log(npc.getShortDesc('en') + ' is on the offensive.');
+            initCombat(l10n, this, p, room, npcs, players, rooms, callback);
+          }
+        });
+
+    }
+  },
+
 };
