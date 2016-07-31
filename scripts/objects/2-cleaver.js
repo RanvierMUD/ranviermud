c43703dc0a9163833733571d4d197d6724ceb7af
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-30 22:48:45 -0400

    Add shiv and cleaver scripts for bonuses and such.

--- scripts/objects/2-cleaver.js
+++ scripts/objects/2-cleaver.js
@@ -3,18 +3,27 @@ exports.listeners = {
   wield: function (l10n) {
     return function (location, player, players) {
       player.say('You ready the weighty cleaver.');
+      player.combat.addToHitMod({
+        name: 'cleaver ' + this.getUuid(),
+        effect: toHit => toHit + 1
+      });
     }
   },
 
   remove: function (l10n) {
     return function (player) {
       player.say('You place the bulky cleaver in your pack.');
+      player.combat.deleteAllMods('cleaver' + this.getUuid());
     }
   },
 
   hit: function (l10n) {
 		return function (player) {
-			player.say('The blade of your cleaver does its job.');
+			player.say('<bold>The blade of your cleaver <red>does its job.</red></bold>');
+      player.combat.addDamageMod({
+        name: 'cleaver' + this.getUuid(),
+        effect: damage => damage + .5
+      });
 		}
 	}
 };
