0b275ee6ffa2400dc24e5722afd2be131348688c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-30 22:48:45 -0400

    Add shiv and cleaver scripts for bonuses and such.

--- scripts/objects/1-shiv.js
+++ scripts/objects/1-shiv.js
@@ -4,18 +4,30 @@ exports.listeners = {
 		return function (location, player, players) {
 			player.say('<yellow>You clench the shiv tightly in your fist.</yellow>');
 			player.equip(location, this);
+			player.combat.addDodgeMod({
+				name: 'shiv' + this.getUuid(),
+				effect: dodge => dodge + 1
+			});
 		}
 	},
 
 	remove: function (l10n) {
 		return function (player) {
 			player.say('You carefully stow the shiv away, avoiding the rusty blade.');
+			player.combat.removeDodgeMod('shiv' + this.getUuid());
 		}
 	},
 
 	hit: function (l10n) {
 		return function (player) {
-			player.say('Your shank finds purchase in flesh.');
+			player.say('<bold>Your shank finds purchase in flesh.</bold>');
+		}
+	},
+
+	parry: function(l10n) {
+		return function(player) {
+			player.say('You deftly knock your opponent\'s strike away with the point of your blade.');
 		}
 	}
+
 };
