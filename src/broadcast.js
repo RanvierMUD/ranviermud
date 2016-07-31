ba331b7f6fd3ec3dd5e4ba6e8eac46a6d1c6b18f
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-07-31 03:16:33 -0400

    Try a different func.

--- src/broadcast.js
+++ src/broadcast.js
@@ -1,15 +1,17 @@
 const Type = require('./type').Type;
+const util = require('util');
 
 const noop = function() {}
 const toRoom = (location, firstParty, secondParty, players) => config => {
-
+  util.log(players);
+  util.log(config);
   const firstPartyMsger = Type.isPlayer(firstParty) ?
     firstParty.say : noop;
   const secondPartyMsger = Type.isPlayer(secondParty) ?
     secondParty.say : noop;
   const location = Type.isPlayer(firstParty) ?
     firstParty.getLocation() : firstParty.getRoom();
-  const thirdPartyMsger = msg => players.eachAt(location, player => player.say(msg));
+  const thirdPartyMsger = msg => players.broadcastAt(location, msg);
 
   firstPartyMsger(config.firstPartyMessage);
   secondPartyMsger(config.secondPartyMessage);
