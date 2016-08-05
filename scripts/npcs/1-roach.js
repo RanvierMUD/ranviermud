135d77d4e4ce06cc2243e4d070fa01a3cf06e5fa
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-08-05 09:35:45 -0400

    Fix broadcast check and fix bad refs in roach script

--- scripts/npcs/1-roach.js
+++ scripts/npcs/1-roach.js
@@ -40,11 +40,11 @@ exports.listeners = {
     return function(room, player, players, hitLocation, damage) {
       const toRoom = Broadcast.toRoom(room, this, player, players);
 
-      const secondPartyMessages = [
+      const secondPartyMessage = [
         'The roach\'s pincers nip your ' + hitLocation + '.',
         'The roach bites your ' + hitLocation + ', drawing a pinprick of blood.',
       ];
-      const thirdPartyMessages = [
+      const thirdPartyMessage = [
         'The roach\'s pincers nip ' + player.combat.getDesc() + '\'s ' + hitLocation + '.',
         'The roach bites ' + player.combat.getDesc() + ' in the ' + hitLocation + ' and a tiny pinprick of blood wells forth.',
       ];
@@ -57,12 +57,12 @@ exports.listeners = {
     return function(room, player, players, hitLocation, damage) {
       const toRoom = Broadcast.toRoom(room, this, player, players);
 
-      const secondPartyMessages = [
+      const secondPartyMessage = [
         'The roach chitters as its antennae are bent at an odd angle.',
         'The roach\'s chitin splinters and cracks around its ' + hitLocation + '.',
         'The roach hisses as ichor oozes from its wounds.'
       ];
-      const thirdPartyMessages = [
+      const thirdPartyMessage = [
         'The roach chitters in pain as its ' + hitLocation + ' shatters.',
         'Hissing furiously, the roach rears its pincer-covered maw.',
         'Ichor oozes from the injured roach.'
