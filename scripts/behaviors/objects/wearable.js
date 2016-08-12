81204b7ae713ad953bcc67ea66983617e57c4289
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-08-12 00:03:01 -0400

    Update wearable and wieldable scripts for behaviors. Turns out behavior scripts are hadny but can also cause errors. if not updated.

--- scripts/behaviors/objects/wearable.js
+++ scripts/behaviors/objects/wearable.js
@@ -1,11 +1,11 @@
 exports.listeners = {
 	wear: function (l10n) {
-		return function (location, player, players) {
+		return function (location, room, player, players) {
 			player.sayL10n(l10n, 'WEAR', this.getShortDesc('en'));
 		};
 	},
 	remove: function (l10n) {
-		return function (player) {
+		return function (room, player, players) {
 			player.sayL10n(l10n, 'REMOVE', this.getShortDesc('en'));
 		};
 	}
