33262cce4398dd7bddb84126d702762e4f9901a2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-08-17 00:28:12 -0400

    Add more flavor.

--- scripts/rooms/15.js
+++ scripts/rooms/15.js
@@ -9,11 +9,11 @@ const util = require('util');
 exports.listeners = {
   playerEnter: l10n => {
     return function splat(player, players, rooms) {
-      const dead = 0;
+      const damage = 220;
       player.say('<bold><white>You <red>splatter</red> against the bottom of the well.</bold></white>');
-      player.setAttribute('health', 0);
+      player.setAttribute('health', player.getAttribute('health') - damage);
       // player.kill();
-      const toArea = Broadcast.toArea(player, players, rooms)
+      const toArea = Broadcast.toArea(player, players, rooms);
       toArea('<white>You hear a series of <bold>sharp crackling sounds.</white></bold>');
     }
   }
