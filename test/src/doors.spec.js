03a1b9af8f4fcaa85534e3889a61ea395e5aa19d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-08-23 21:48:41 -0400

    Set up dests for helper funcs in doors.js

--- test/src/doors.spec.js
+++ test/src/doors.spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const expect = require('chai').expect;
 const Room = require('../../src/rooms').Room;
 const Doors = require('../../src/doors').Doors;
@@ -7,9 +9,42 @@ const testRoom = new Room(Mocks.Room);
 
 describe('Doors & Locks', () => {
 
-  it('Should find an exit given a direction', () => {
-    const found = Doors.findExit(testRoom, 'out');
-    expect(found.length === 1).to.be.true;
+
+  describe('findExit', () => {
+
+    it('Should find an exit given a direction', () => {
+      const found = Doors.findExit(testRoom, 'out');
+      expect(found.length === 1).to.be.true;
+    });
+
+    it('Should not find an exit if the direction doesn\'t exist', () => {
+      const found = Doors.findExit(testRoom, 'wat');
+      expect(found.length === 0).to.be.true;
+    });
+
   });
+
+  describe('updateDestination', () => {
+    const getLocation = () => '1';
+    const player = { getLocation };
+    const dest = {
+      getExits: () => [{
+        location: '1'
+      }]
+    };
+
+    it('should call a callback if the exit exists', () => {
+      let called = false;
+      Doors.updateDestination(player, dest, () => {
+        called = true;
+      });
+
+      expect(called).to.be.true;
+
+    });
+
+
+  });
+
 
 });
