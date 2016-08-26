be50be3698206c879b3fb94f46be7a170654522a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-08-26 11:35:01 -0400

    Add test for checking npc passage and fix naughty code.

--- src/doors.js
+++ src/doors.js
@@ -28,7 +28,7 @@ const defaultDifficulty = 10;
 const getLockDifficulty = exit => parseInt(exit.door.difficulty || defaultDifficulty, 10);
 
 /* Dealing with npc passage */
-const isMobLocked   = exit => exit.hasOwnProperty('mob_locked');
+const isMobLocked   = exit => exit.door.mob_locked && exit.door.mob_locked === true;
 const isNpcPassable = exit => !(isMobLocked(exit) || isLocked(exit));
 
 /* Dealing with open/closed states */
