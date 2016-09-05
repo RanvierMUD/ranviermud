0a86e218796f32ed0c9cd78914e130da3ab4bb59
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-04 20:56:25 -0400

    Wrote a failing test and a helper function for breaking a sentence into words.

--- test/src/dialogue.spec.js
+++ test/src/dialogue.spec.js
@@ -1,6 +1,21 @@
 const Dialogue = require('../../src/dialogue').Dialogue;
 const expect = require('chai').expect;
 
-describe('it should be a thing', () => {
-  expect(Dialogue).to.be.ok;
+describe('Basic keyword parsing', () => {
+
+  const mockConfig = {
+    'thieves guild': {
+      keywords: ['thieves', 'thief', 'stealing', 'guild'],
+      priority: 4,
+      prereqs: {
+        introduced: true
+      },
+      dialogue: 'I need you to infiltrate the thieves guild for me, and find their roster.'
+    }
+  };
+
+
+  it('should be true if the keyword is in the string', () => {
+    expect(Dialogue.hasKeyword('thief', mockConfig)).to.be.true;
+  });
 });
