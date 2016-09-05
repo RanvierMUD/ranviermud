5a7496740def9a887bca63b5f205c074eefae531
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-04 20:56:25 -0400

    Wrote a failing test and a helper function for breaking a sentence into words.

--- src/dialogue.js
+++ src/dialogue.js
@@ -1,5 +1,9 @@
+const hasKeyword = (sentence, config) => {
+  const tokens = tokenizeSentence(sentence);
+};
 
+const tokenizeSentence = sentence => sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').split(' ');
 
 exports.Dialogue = {
-
+  hasKeyword, tokenizeSentence
 };
