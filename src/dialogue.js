c43aa9a16f34c7e6dc6d9dca19f4e8219bf762c7
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-05 01:50:22 -0400

    Returns true if the sentence has keywords, else false.

--- src/dialogue.js
+++ src/dialogue.js
@@ -1,6 +1,26 @@
-const hasKeyword = (sentence, config) => {
-  const tokens = tokenizeSentence(sentence);
+'use strict';
+
+const _ = require('./helpers');
+
+const hasKeyword = (tokens, topic) => {
+  const keywords = topic.keywords;
+  const found = keywords.reduce((found, keyword) => found || _.has(tokens, keyword), false);
+  return found;
 };
+
+const parseSentence = (sentence, config) => {
+  const tokens = tokenizeSentence(sentence);
+  const topics = [];
+
+  for (let topic in config) {
+    const found = hasKeyword(tokens, config[topic]);
+    if (found) { topics.push(topic); }
+  }
+
+
+};
+
+
 
 const tokenizeSentence = sentence => sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').split(' ');
 
