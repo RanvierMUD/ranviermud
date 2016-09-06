cafdbf28aac8f830ed4a3678d19aa36fd7766a52
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-06 19:17:06 -0400

    Organize tests a bit more, add test for sad path.

--- test/src/dialogue.spec.js
+++ test/src/dialogue.spec.js
@@ -3,7 +3,7 @@
 const Dialogue = require('../../src/dialogue').Dialogue;
 const expect = require('chai').expect;
 
-describe('Basic keyword parsing', () => {
+describe.only('Basic keyword parsing', () => {
 
   const mockConfig = {
     'thieves guild': {
@@ -16,20 +16,33 @@ describe('Basic keyword parsing', () => {
     }
   };
 
-  it('should be able to break a string into words', () => {
-    expect(Dialogue.tokenizeSentence('hello world!')).to.eql(['hello', 'world']);
+  describe('tokenization', () => {
+    it('should be able to break a string into words', () => {
+      expect(Dialogue.tokenizeSentence('hello world!')).to.eql(['hello', 'world']);
+    });
   });
 
-  it('should be true if the keyword is in the string', () => {
-    expect(Dialogue.hasKeyword('thief', mockConfig['thieves guild'])).to.be.true;
+  describe('keyword finding', () => {
+    it('should be true if the keyword is in the string', () => {
+      expect(Dialogue.hasKeyword('thief', mockConfig['thieves guild'])).to.be.true;
+    });
+
+    it('should be false if the keyword is not in the string', () => {
+      expect(Dialogue.hasKeyword('potatoes', mockConfig['thieves guild'])).to.be.false;
+    });
+
   });
 
-  it('should be false if the keyword is not in the string', () => {
-    expect(Dialogue.hasKeyword('potatoes', mockConfig['thieves guild'])).to.be.false;
+  describe('finding topics', () => {
+    it('should return a list of a single topic if there is only one', () => {
+      const tokens = Dialogue.tokenizeSentence('thieves guild');
+      expect(Dialogue.findPotentialTopics(tokens, mockConfig).length === 1).to.be.true;
+    });
+
+    it('should return an empty array if there are no relevant topics', () => {
+      const tokens = Dialogue.tokenizeSentence('pants helicopter');
+      expect(Dialogue.findPotentialTopics(tokens, mockConfig).length === 0).to.be.true;
+    });
   });
 
-  it('should return a list of a single topic if there is only one', () => {
-    const tokens = Dialogue.tokenizeSentence('thieves guild');
-    expect(Dialogue.findPotentialTopics(tokens, mockConfig).length === 1).to.be.true;
-  });
 });
