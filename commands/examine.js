7e555ec541e974ba9c7e0692cdff74780e99061d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-11 00:05:48 -0400

    Derp mcflerp

--- commands/examine.js
+++ commands/examine.js
@@ -1,8 +1,8 @@
 'use strict';
 const l10nFile = __dirname + '/../l10n/commands/examine.yml';
 const l10n = require('../src/l10n')(l10nFile);
-const CommandUtil = require('../src/command_util')
-  .CommandUtil;
+const CommandUtil = require('../src/command_util').CommandUtil;
+
 const util = require('util');
 
 exports.command = (rooms, items, players, npcs, Commands) => {
@@ -17,8 +17,7 @@ exports.command = (rooms, items, players, npcs, Commands) => {
       util.log(player.getName() + ' is searching for ' + args);
       util.log('  in ' + room.getTitle('en'));
 
-      room.emit('examine', args, player, players);
-      return;
+      if (CommandUtil.hasScript(room, 'examine')) { return room.emit('examine', args, player, players); }
     }
 
     return player.sayL10n(l10n, 'NOTHING_EXAMINED');
