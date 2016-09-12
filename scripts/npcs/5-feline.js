034b8ca881d740e58d7351ad079240ced6b79e07
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-12 18:01:42 -0400

    More improvements to cat scripts, level it up because I'm planning on adding cool behaviors to the combat script such as its dodge improving every time the player misses, and its toHit decreasing if the player hits it in the head. Make it more of a badass in combat but not invincible. Should do similar stuff with kazuul.

--- scripts/npcs/5-feline.js
+++ scripts/npcs/5-feline.js
@@ -49,25 +49,31 @@ exports.listeners = {
       const playerName = player.getShortDesc('en');
 
       let secondPartyMessage, thirdPartyMessage;
-      if (hitLocation === 'head') {
-        secondPartyMessage = 'The tomcat flies at your face, a ball of fur, his claws raking your eyes.';
-        thirdPartyMessage = 'The tomcat flies at '+ playerName + '\'s face, a ball of fur, his claws raking their eyes.';
 
-      } else if (hitLocation === 'neck') {
-        secondPartyMessage = 'The ginger cat tears at your throat, screeching.';
-        thirdPartyMessage = 'The ginger cat tears at ' + playerName + '\'s throat, screeching.';
+      //TODO: Extract this to some kind of combat messaging helper?
+      switch(hitLocation) {
+        case 'head':
+          secondPartyMessage = 'The tomcat flies at your face, a ball of fur, his claws raking your eyes.';
+          thirdPartyMessage = 'The tomcat flies at '+ playerName + '\'s face, a ball of fur, his claws raking their eyes.';
+          break;
 
-      } else {
-        secondPartyMessage = [
-          'The tomcat\'s claws rake your ' + hitLocation + ', drawing lines of blood.',
-          'The cat fiercely nips your ' + hitLocation + '.',
-          'A screeching ball of fur, the tomcat sinks its claws into your ' + hitLocation + '.'
-        ];
-        thirdPartyMessage = [
-          'The tomcat rakes ' + playerName + '\'s' + hitLocation + ' with his sharpened claws.',
-          'The cat quickly nips ' + playerName + '\'s ' + hitLocation + ' and darts away, an orange flash.',
-          'The cat flies through the air at ' + playerName + ' and sinks his claws in their ' + hitLocation + '.'
-        ];
+        case 'neck':
+          secondPartyMessage = 'The ginger cat tears at your throat, screeching.';
+          thirdPartyMessage = 'The ginger cat tears at ' + playerName + '\'s throat, screeching.';
+          break;
+
+        default:
+          secondPartyMessage = [
+            'The tomcat\'s claws rake your ' + hitLocation + ', drawing lines of blood.',
+            'The cat fiercely nips your ' + hitLocation + '.',
+            'A screeching ball of fur, the tomcat sinks its claws into your ' + hitLocation + '.'
+          ];
+          thirdPartyMessage = [
+            'The tomcat rakes ' + playerName + '\'s' + hitLocation + ' with his sharpened claws.',
+            'The cat quickly nips ' + playerName + '\'s ' + hitLocation + ' and darts away, an orange flash.',
+            'The cat flies through the air at ' + playerName + ' and sinks his claws in their ' + hitLocation + '.'
+          ];
+          break;
       }
 
       Broadcast.consistentMessage(toRoom, { secondPartyMessage, thirdPartyMessage });
@@ -82,23 +88,23 @@ exports.listeners = {
 
       const secondPartyMessage = moderateDamage ?
       [
-        'The rat screeches in pain, a bloody foam spraying from its maw.',
-        'A gash opens across the feral rat\'s matted fur.',
-        'The rabid rodent is knocked back by the blow, a ball of furry fury.',
-        'The rat\'s ' + hitLocation + ' tears under the force of the blow.'
+        'The cat screeches in anguish.',
+        'The tomcat leaps back, to lick his wounds for a second.',
+        'The tomcat is knocked back by the blow, his monocle popping loose for a second.',
+        'The cat\'s ' + hitLocation + ' is bruised by your strike.'
       ] :
       [
-        'The rat\'s '+ hitLocation + ' is crushed, a matted ball of bloody fur and bones.'
+        'The tomcat\'s '+ hitLocation + ' is smashed open, and his blood spatters on the floor.'
       ];
       const thirdPartyMessage = moderateDamage ?
       [
-        'The rat screeches in pain as its ' + hitLocation + ' crumples.',
-        'Foaming at the maw, the rat is gashed by ' + player.getShortDesc('en') + '\'s attack.',
-        'The fierce rat is bashed away by ' + player.getShortDesc('en') + '.',
-        'Blood sprays across the ground as the feral rat\'s ' + hitLocation + ' is sundered.'
+        'The cat screeches in pain as its ' + hitLocation + ' is bruised.',
+        'The tomcat retreats after ' + player.getShortDesc('en') + '\'s vicious attack, to lick his wounds.',
+        'The tomcat is bashed away by ' + player.getShortDesc('en') + ', and his monocle pops loose.',
+        'The tomcat\'s ' + hitLocation + ' is bashed, and begins swelling immediately.'
       ] :
       [
-        'The rat\'s '+ hitLocation + ' is crushed, a matted ball of bloody fur, thanks to ' + player.getShortDesc('en') + '.'
+        'The cat\'s '+ hitLocation + ' is smashed to a furry ginger pulp, thanks to ' + player.getShortDesc('en') + '.'
       ];
 
       Broadcast.consistentMessage(toRoom, { secondPartyMessage, thirdPartyMessage });
@@ -110,14 +116,14 @@ exports.listeners = {
       const toRoom = Broadcast.toRoom(room, this, player, players);
 
       const secondPartyMessage = [
-        'The rat scurries away from you.',
-        'Your attack hits nothing but fur.',
-        'The rabid rodent leaps past you as you make your move, and you miss.',
+        'The cat leaps away from your strike, and bows ironically.',
+        'Your attack hits nothing but fur as the cat darts past you.',
+        'The tomcat jumps out of the way, and you swear it laughs at you.',
       ];
       const thirdPartyMessage = [
-        'The rat scurries away from ' + player.getShortDesc('en') + '\'s attack.',
-        player.getShortDesc('en') + '\'s attack hits only the rat\'s fur.',
-        'The fierce rat leaps past ' + player.getShortDesc('en') + ' and they whiff.',
+        'The cat leaps away from ' + player.getShortDesc('en') + '\'s attack.',
+        player.getShortDesc('en') + '\'s attack hits nothing as the tomcat runs circles around them.',
+        'The sassy tomcat jumps away ' + player.getShortDesc('en') + ', causing them to miss his ' + hitLocation + '.',
       ];
 
       Broadcast.consistentMessage(toRoom, { secondPartyMessage, thirdPartyMessage });
@@ -129,14 +135,14 @@ exports.listeners = {
       const toRoom = Broadcast.toRoom(room, this, player, players);
 
       const secondPartyMessage = [
-        '<yellow>The rat leaps for your ' + hitLocation + ', but flies past in a ball of fur and fangs.</yellow>',
-        '<yellow>The rat\'s claws bounce harmlessly off your ' + hitLocation + '.</yellow>',
-        '<yellow>The rabid rodent sprays bloody foam on your ' + hitLocation + ' as they try to bite and miss.</yellow>',
+        '<yellow>The tomcat leaps for your ' + hitLocation + ', but flies past in a blur of claws.</yellow>',
+        '<yellow>The tomcat\'s claws miss your ' + hitLocation + '.</yellow>',
+        '<yellow>The cat adjusts his monocle after missing your ' + hitLocation + '.</yellow>',
       ];
       const thirdPartyMessage = [
-        '<yellow>The rat leaps for ' + player.getShortDesc('en') + '\s ' + hitLocation + ', but flies past in a ball of fur and fangs.</yellow>',
-        '<yellow>The rat claws uselessly at ' + player.getShortDesc('en') + ', missing.</yellow>',
-        '<yellow>The rabid critter sprays bloody foam on ' + player.getShortDesc('en') + ' but fails to do any real damage.</yellow>',
+        '<yellow>The tomcat leaps for ' + player.getShortDesc('en') + '\s ' + hitLocation + ', but flies past in a blur of claws.</yellow>',
+        '<yellow>The tomcat claws at ' + player.getShortDesc('en') + ' but whiffs.</yellow>',
+        '<yellow>The classy cat adjusts his monocle after missing ' + player.getShortDesc('en') + '.</yellow>',
       ];
 
       Broadcast.consistentMessage(toRoom, { secondPartyMessage, thirdPartyMessage });
