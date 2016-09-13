f83d91df04b028d2198db653584473311e570ad8
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-09-13 01:31:09 -0400

    Add stacking monoc1e breaking hit, improve messaging, show name in combat prompt if intro'ed, etc

--- scripts/npcs/5-feline.js
+++ scripts/npcs/5-feline.js
@@ -48,30 +48,39 @@ exports.listeners = {
       const toRoom = Broadcast.toRoom(room, this, player, players);
       const playerName = player.getShortDesc('en');
 
+      this.combat.addSpeedMod({
+        name: 'ferocious feline',
+        effect: speed => Math.round(speed * .75)
+      });
+      this.combat.addDamageMod({
+        name: 'ferocious feline',
+        effect: damage => damage + 10
+      });
+
       let secondPartyMessage, thirdPartyMessage;
 
       //TODO: Extract this to some kind of combat messaging helper?
       switch(hitLocation) {
         case 'head':
-          secondPartyMessage = 'The tomcat flies at your face, a ball of fur, his claws raking your eyes.';
-          thirdPartyMessage = 'The tomcat flies at '+ playerName + '\'s face, a ball of fur, his claws raking their eyes.';
+          secondPartyMessage = ['The tomcat flies at your face, a ball of fur, his claws <red>raking</red> your eyes.'];
+          thirdPartyMessage = ['The tomcat flies at '+ playerName + '\'s face, a ball of fur, his claws <red>raking</red> their eyes.'];
           break;
 
         case 'neck':
-          secondPartyMessage = 'The ginger cat tears at your throat, screeching.';
-          thirdPartyMessage = 'The ginger cat tears at ' + playerName + '\'s throat, screeching.';
+          secondPartyMessage = ['The ginger cat <red>tears</red> at your throat, screeching.'];
+          thirdPartyMessage = ['The ginger cat <red>tears</red> at ' + playerName + '\'s throat, screeching.'];
           break;
 
         default:
           secondPartyMessage = [
-            'The tomcat\'s claws rake your ' + hitLocation + ', drawing lines of blood.',
-            'The cat fiercely nips your ' + hitLocation + '.',
-            'A screeching ball of fur, the tomcat sinks its claws into your ' + hitLocation + '.'
+            'The tomcat\'s claws rake your ' + hitLocation + ', drawing <red>lines</led> <white>of</white> <red>blood</red>.',
+            'The cat fiercely <yellow>nips</yellow> your ' + hitLocation + '.',
+            'A <bold>screeching</bold> ball of fur, the tomcat sinks its claws into your ' + hitLocation + '.'
           ];
           thirdPartyMessage = [
-            'The tomcat rakes ' + playerName + '\'s' + hitLocation + ' with his sharpened claws.',
-            'The cat quickly nips ' + playerName + '\'s ' + hitLocation + ' and darts away, an orange flash.',
-            'The cat flies through the air at ' + playerName + ' and sinks his claws in their ' + hitLocation + '.'
+            'The tomcat <red>rakes</red> ' + playerName + '\'s' + hitLocation + ' with his sharpened claws.',
+            'The cat quickly nips ' + playerName + '\'s ' + hitLocation + ' and darts away, an orange <yellow>flash</yellow>.',
+            'The cat <bold>flies</bold> through the air at ' + playerName + ' and sinks his claws in their ' + hitLocation + '.'
           ];
           break;
       }
@@ -86,25 +95,33 @@ exports.listeners = {
 
       const moderateDamage = damage < this.getAttribute('health') - 1;
 
+      if (hitLocation === 'head' && damage > 10) {
+        player.say('<bold>You whack the cat in the head and his monocle cracks.</bold>');
+        this.combat.addToHitMod({
+          name: 'monocle broke' + damage,
+          effect: toHit => toHit - 3
+        });
+      }
+
       const secondPartyMessage = moderateDamage ?
       [
         'The cat screeches in anguish.',
-        'The tomcat leaps back, to lick his wounds for a second.',
-        'The tomcat is knocked back by the blow, his monocle popping loose for a second.',
-        'The cat\'s ' + hitLocation + ' is bruised by your strike.'
+        'The tomcat leaps back, to lick his <red>wounds</red> for a second.',
+        'The tomcat is knocked back by the blow, his monocle <bold><cyan>popping</cyan></bold> loose for a second.',
+        'The cat\'s ' + hitLocation + ' is <blue>bruised</blue> by your strike.'
       ] :
       [
-        'The tomcat\'s '+ hitLocation + ' is smashed open, and his blood spatters on the floor.'
+        'The tomcat\'s '+ hitLocation + ' is smashed open, and his <red>blood</red> spatters on the floor.'
       ];
       const thirdPartyMessage = moderateDamage ?
       [
-        'The cat screeches in pain as its ' + hitLocation + ' is bruised.',
-        'The tomcat retreats after ' + player.getShortDesc('en') + '\'s vicious attack, to lick his wounds.',
-        'The tomcat is bashed away by ' + player.getShortDesc('en') + ', and his monocle pops loose.',
-        'The tomcat\'s ' + hitLocation + ' is bashed, and begins swelling immediately.'
+        'The cat screeches in pain as its ' + hitLocation + ' is <blue>bruised</blue>.',
+        'The tomcat retreats after ' + player.getShortDesc('en') + '\'s vicious attack, to lick his <red>wounds</red>.',
+        'The tomcat is bashed away by ' + player.getShortDesc('en') + ', and his monocle <bold>pops</bold> loose.',
+        'The tomcat\'s ' + hitLocation + ' is bashed, and begins <blue>swelling</blue> immediately.'
       ] :
       [
-        'The cat\'s '+ hitLocation + ' is smashed to a furry ginger pulp, thanks to ' + player.getShortDesc('en') + '.'
+        'The cat\'s '+ hitLocation + ' is smashed to a furry ginger <red>pulp</red>, thanks to ' + player.getShortDesc('en') + '.'
       ];
 
       Broadcast.consistentMessage(toRoom, { secondPartyMessage, thirdPartyMessage });
@@ -133,6 +150,8 @@ exports.listeners = {
   missedAttack: function(l10n) {
     return function(room, player, players, hitLocation, damage) {
       const toRoom = Broadcast.toRoom(room, this, player, players);
+
+      this.combat.removeAllMods('ferocious feline');
 
       const secondPartyMessage = [
         '<yellow>The tomcat leaps for your ' + hitLocation + ', but flies past in a blur of claws.</yellow>',
