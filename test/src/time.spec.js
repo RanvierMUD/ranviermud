8dd556fc14d177b0fa34ae43f49015c83e778b5b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-02 20:07:25 -0400

    Test weather emitting business.

--- test/src/time.spec.js
+++ test/src/time.spec.js
@@ -1,8 +1,12 @@
 const expect = require('chai').expect;
 const sinon  = require('sinon');
 const Time = require('../../src/time.js').Time;
+const Random = require('../../src/random.js').Random;
 
 describe('checking to see if it is day or night', () => {
+
+  afterEach(function () { this.clock.restore(); });
+
   it('should return day for an even-numbered hours', function() {
     this.clock = sinon.useFakeTimers();
     const isDay = Time.isDay();
@@ -12,7 +16,43 @@ describe('checking to see if it is day or night', () => {
   it('should return night for odd numbered hours', function() {
     this.clock = sinon.useFakeTimers();
     this.clock.tick(61 * 60 * 1000);
-    const isDay = Time.isDay();
-    expect(isDay).to.be.false;
+    const isNight = Time.isNight();
+    expect(isNight).to.be.true;
   });
+
+});
+
+describe('weather stuff', () => {
+
+  const fakeRoom = {
+    getBiome: () => 'outdoors'
+  };
+  const fakeRooms = {
+    getAt: () => fakeRoom
+  };
+  const fakePlayer = {
+    getLocation: () => {},
+    say: sinon.spy()
+  }
+  const fakePlayers = {
+    filter: cb => [ fakePlayer ].filter(cb)
+  };
+
+
+  const possibleWeatherEvents = [
+    'The clouds part for a second.',
+    'Rain spatters against the ground.'
+  ]; //.map(s => '<blue>' + s + '</blue>');
+
+  // Weather will always emit
+  Random.roll = sinon.stub().returns(99);
+
+  it('should emit weather', function() {
+    // Should be day
+    this.clock = sinon.useFakeTimers();
+
+    const emitted = Time.checkWeather(fakeRooms, fakePlayers);
+    expect(possibleWeatherEvents.indexOf(emitted) > -1).to.be.true;
+  });
+
 });
