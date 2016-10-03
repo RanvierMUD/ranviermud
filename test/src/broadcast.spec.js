46175138975d8dfde187c081711e917377584070
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-03 15:44:14 -0400

    Add test for toRoom broadcaster.

--- test/src/broadcast.spec.js
+++ test/src/broadcast.spec.js
@@ -2,12 +2,19 @@ const expect = require('chai').expect;
 const sinon  = require('sinon');
 
 const Broadcast = require('../../src/broadcast').Broadcast;
+const Player = require('../../src/player').Player;
+const Npc = require('../../src/npcs').Npc;
+
+const Type   = require('../../src/type.js').Type;
+Type.config(Player, Npc);
 
 describe('broadcasting to an area', () => {
+
   it('should return a broadcaster function', () => {
     const fn = Broadcast.toArea({}, {}, {});
     expect(typeof fn === 'function').to.be.true;
   });
+
   it('should say a thing to all players in area except originator', () => {
     const fakePlayer = {
       say: sinon.spy(),
@@ -36,5 +43,49 @@ describe('broadcasting to an area', () => {
 
     expect(fakeOtherPlayer.say.calledWith('test message')).to.be.true
 
-  })
+  });
+
+});
+
+
+describe('broadcasting to a single room', () => {
+
+  const fakePlayer = {
+    say: sinon.spy(),
+    prompt: sinon.spy(),
+    getLocation: () => 2
+  };
+
+  const firstParty       = Object.assign(new Player(), fakePlayer);
+  const secondParty      = Object.assign(new Player(), fakePlayer);
+  const thirdPartyPlayer = Object.assign(new Player(), fakePlayer);
+  const thirdParty = {
+    eachIf: (condition, cb) => {
+      if (condition(thirdPartyPlayer)) { cb(thirdPartyPlayer); }
+    }
+  };
+
+  const fakeRoom = {
+    getLocation: () => 2
+  };
+
+  it('should return a broadcaster function', () => {
+    const fn = Broadcast.toRoom({}, {}, {}, {});
+    expect(typeof fn === 'function').to.be.true;
+  });
+
+  it('should say things to all parties involved', () => {
+    const toRoom = Broadcast.toRoom(fakeRoom, firstParty, secondParty, thirdParty);
+    const messages = {
+      firstPartyMessage:  '1st test',
+      secondPartyMessage: '2nd test',
+      thirdPartyMessage:  '3rd test'
+    };
+    toRoom(messages);
+
+    expect(firstParty.say.calledWith('1st test')).to.be.true;
+    expect(secondParty.say.calledWith('2nd test')).to.be.true;
+    expect(thirdPartyPlayer.say.calledWith('3rd test')).to.be.true;
+  });
+
 });
