2af6ae9d9e998f5ad76df806ea98e51d936e40b6
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-11 18:29:35 -0400

    Test for directory strings.

--- test/src/items.spec.js
+++ test/src/items.spec.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 
 const Items = require('../../src/items').Items;
-const Item = require('../../src/item').Item;
+const Item = require('../../src/items').Item;
 
 const testItemConfig = {
   short_description: 'potato',
@@ -19,6 +19,11 @@ const createTestItems = config => Array(5).fill({}).map(_ => new Item(config));
 describe('Item manager', () => {
   const items = new Items();
   const testItems = createTestItems(testItemConfig);
+
+  describe('should be able to get directory strings', () =>{
+    expect(typeof items.getScriptsDir() === 'string').to.be.true;
+    expect(typeof items.getL10nDir() === 'string').to.be.true;
+  });
 
 });
 
