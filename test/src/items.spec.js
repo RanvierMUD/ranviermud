6065b100fb4bad6211601682a8e14503792b5fbe
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-11 17:00:25 -0400

    Set up items test objs, make misc fixes to items.js

--- test/src/items.spec.js
+++ test/src/items.spec.js
@@ -5,9 +5,20 @@ const expect = require('chai').expect;
 const Items = require('../../src/items').Items;
 const Item = require('../../src/item').Item;
 
+const testItemConfig = {
+  short_description: 'potato',
+  keywords:          ['potato', 'spud'],
+  description:       'a potato',
+  inventory:         [],
+  room:              2,
+  vnum:              4
+};
+const createTestItems = config => Array(5).fill({}).map(_ => new Item(config));
+
+
 describe('Item manager', () => {
   const items = new Items();
-
+  const testItems = createTestItems(testItemConfig);
 
 });
 
