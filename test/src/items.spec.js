a4b773161f6f6ffdb4d0bf11c332e42d390e823d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-10 20:54:04 -0400

    Add item test file skeleton.

---
+++ test/src/items.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const Items = require('../../src/items').Items;
+const Item = require('../../src/item').Item;
+
+describe('Item manager', () => {
+  const items = new Items();
+
+
+});
+
+describe('Item class', () => {
+  const testItem = new Item({});
+});
