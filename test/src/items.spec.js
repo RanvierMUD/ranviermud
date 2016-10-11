e0b3deb6e030e67021afaa28e9193523dbd02152
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-11 18:37:31 -0400

    Add test for items being added and having uuids added.

--- test/src/items.spec.js
+++ test/src/items.spec.js
@@ -20,9 +20,19 @@ describe('Item manager', () => {
   const items = new Items();
   const testItems = createTestItems(testItemConfig);
 
-  describe('should be able to get directory strings', () =>{
+  it('should be able to get directory strings', () =>{
     expect(typeof items.getScriptsDir() === 'string').to.be.true;
     expect(typeof items.getL10nDir() === 'string').to.be.true;
+  });
+
+  it('should be able to add items and set uuid', () => {
+    testItems.forEach(item => items.addItem(item));
+    let count = 0;
+    items.each(item => {
+      count++;
+      expect(item.getUuid()).to.be.ok;
+    });
+    expect(count === 5).to.be.true;
   });
 
 });
