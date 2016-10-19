efa40411ce735e5003fe2a6ac2088177af38a480
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-18 21:30:44 -0400

    Copy and paste script into wield to test it.

--- scripts/behaviors/objects/wieldable.js
+++ scripts/behaviors/objects/wieldable.js
@@ -3,23 +3,79 @@ const LevelUtil = require('../../../src/levels').LevelUtil;
 
 
 exports.listeners = {
-	hit: function (l10n) {
-		return function (room, attacker, defender, players, hitLocation, damageDealt) {
-			checkForCrit(attacker, defender, damageDealt);
-		}
-	},
+  hit: function (l10n) {
+    return function (room, attacker, defender, players, hitLocation, damageDealt) {
+      checkForCrit(attacker, defender, damageDealt);
+    }
+  },
+
+  wield: function (l10n) {
+    return function (location, room, player, players) {
+      if (missedPrerequisites.length) {
+
+				//TODO: Adjust or extract this as it is just copied and pasted from the default wear scripto.
+        missedPrerequisites.forEach(prereq => {
+          switch (prereq) {
+            case 'stamina':
+              const factor = player.getAttribute('stamina') / this.getPrerequisite('stamina'); //TODO: Extract
+              player.say('You are not strong enough to wear this properly.');
+              player.addEffect('encumbered_by_' + this.getShortDesc() + location, Effects.encumbered({ player, factor }));
+              player.combat.addSpeedMod({
+                name: 'encumbered_by_' + this.getShortDesc() + '_' + location,
+                effect: speed => speed * factor //TODO: Extract
+              });
+              break;
+            case 'quickness':
+              const factor = player.getAttribute('quickness') / this.getPrerequisite('quickness');
+
+              player.say('You are not quick enough to move about deftly in this.');
+              player.combat.addDodgeMod({
+                name: 'slowed_by_' + this.getShortDesc() + '_' + location, //TODO: Extract
+                effect: dodge => dodge * factor
+              });
+
+              break;
+            case 'cleverness':
+              const factor = player.getAttribute('cleverness') / this.getPrerequisite('cleverness');
+
+              player.say('You are not sure how to handle this piece of gear...');
+              player.combat.addToHitMod({
+                name: 'confused_by_' + this.getShortDesc() + '_' + location,
+                effect: toHit => toHit * factor
+              });
+
+              break;
+            case 'willpower':
+              const factor = player.getAttribute('cleverness') / this.getPrerequisite('cleverness');
+
+              player.say('You find yourself easily distracted as you don the ' + this.getShortDesc());
+              player.combat.addDefenseMod({
+                name: 'distracted_by_' + this.getShortDesc() + '_' + location,
+                effect: defense => defense * factor
+              });
+
+              break;
+            default:
+              player.say('You have some trouble putting that on...');
+
+          }
+        });
+
+      }
+    }
+  }
 
 };
 
 function checkForCrit(attacker, defender, damageDealt) {
-	var defenderHealth    = defender.getAttribute('health');
-	var defenderMaxHealth = defender.getAttribute('max_health');
+  var defenderHealth = defender.getAttribute('health');
+  var defenderMaxHealth = defender.getAttribute('max_health');
 
-	var massiveDamage = damageDealt > defenderMaxHealth * .5;
-	var almostDead  = defenderHealth <= defenderMaxHealth * .2;
-	var coupDeGrace = almostDead && damageDealt >= defenderHealth;
+  var massiveDamage = damageDealt > defenderMaxHealth * .5;
+  var almostDead = defenderHealth <= defenderMaxHealth * .2;
+  var coupDeGrace = almostDead && damageDealt >= defenderHealth;
 
-	if (massiveDamage || coupDeGrace) {
-		attacker.say('<bold><cyan>You have dealt a critical blow!</cyan></bold>');
-	}
+  if (massiveDamage || coupDeGrace) {
+    attacker.say('<bold><cyan>You have dealt a critical blow!</cyan></bold>');
+  }
 }
