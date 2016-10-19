81fdcb701030fca18dc218a5ddb54205f0b9541b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-19 17:44:40 -0400

    Add effect cleanup and remove emit to wieldable scripts.

--- scripts/behaviors/objects/wieldable.js
+++ scripts/behaviors/objects/wieldable.js
@@ -30,4 +30,24 @@ exports.listeners = {
     }
   },
 
+  remove: function (l10n) {
+    return function (location, room, player, players) {
+      const toRoom = Broadcast.toRoom(room, player, null, players);
+      const desc = this.getShortDesc('en');
+      const name = player.getName();
+      Broadcast.consistentMessage(toRoom, {
+        firstPartyMessage: 'You remove the ' + desc + '.',
+        thirdPartyMessage: name + ' removes the ' + desc + '.'
+      });
+
+      // Remove penalties that may have been added.
+      player.removeEffect('encumbered_by_' + this.getShortDesc() + location);
+      player.removeEffect('confused_by_' + this.getShortDesc() + location);
+
+      player.combat.deleteAllMods('distracted_by_' + this.getShortDesc() + '_' + location);
+      player.combat.deleteAllMods('encumbered_by_' + this.getShortDesc() + '_' + location);
+      player.combat.deleteAllMods('slowed_by_' + this.getShortDesc() + '_' + location);
+    };
+  }
+
 };
