1d2a578cb6fc121c69f1accc6e57300c8ba8fe2d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-19 13:13:20 -0400

    Fix references to this, remove finished TODO, add whitespace for readability.

--- src/item_util.js
+++ src/item_util.js
@@ -1,48 +1,54 @@
 const util = require('util');
 
 const penalize = (player, attr, callback) => {
-  const factor = player.getAttribute(attr) / this.getPrerequisite(attr);
+  const factor = player.getAttribute(attr) / item.getPrerequisite(attr);
   callback(factor);
 }
 
 const getPenaltyDesc = (item, location, status) => status + '_by_' + item.getShortDesc() + '_' + location;
 
 const useDefaultPenalties = (item, player, location, missedPrerequisites) => {
-  //TODO: Adjust or extract this as it is just copied and pasted from the default wear scripto.
+
   missedPrerequisites.forEach(prereq => {
+
     switch (prereq) {
+
       case 'stamina':
         return penalize(player, 'stamina', factor => {
-          const name = getPenaltyDesc(this, location, 'encumbered');
-          player.warn('You are not strong enough to wear this properly.');
+          const name = getPenaltyDesc(item, location, 'encumbered');
+          player.warn('You are not strong enough to wear item properly.');
           player.addEffect(name , Effects.encumbered({ player, factor }));
           player.combat.addSpeedMod({ name, effect: speed => speed * factor });
         });
+
       case 'quickness':
         return penalize(player, 'quickness', factor => {
-          const name = getPenaltyDesc(this, location, 'slowed');
-          player.warn('You are not quick enough to move about deftly in this.');
+          const name = getPenaltyDesc(item, location, 'slowed');
+          player.warn('You are not quick enough to move about deftly in item.');
           player.combat.addDodgeMod({ name, effect: dodge => dodge * factor });
         });
+
       case 'cleverness':
         return penalize(player, 'cleverness', factor => {
-          player.say('You are not sure how to handle this piece of gear...');
+          player.say('You are not sure how to handle item piece of gear...');
           player.combat.addToHitMod({
-            name: 'confused_by_' + this.getShortDesc() + '_' + location,
+            name: 'confused_by_' + item.getShortDesc() + '_' + location,
             effect: toHit => toHit * factor
           });
         });
+
       case 'willpower':
         return penalize(player, 'willpower', factor => {
-          player.say('You find yourself easily distracted as you don the ' + this.getShortDesc());
+          player.say('You find yourself easily distracted as you don the ' + item.getShortDesc());
           player.combat.addDefenseMod({
-            name: 'distracted_by_' + this.getShortDesc() + '_' + location,
+            name: 'distracted_by_' + item.getShortDesc() + '_' + location,
             effect: defense => defense * factor
           });
         });
+
       default:
         player.say('You have some trouble putting that on...');
-        util.log('ITEM ' + this.getShortDesc() + ' has unsupported prerequisites.');
+        util.log('ITEM ' + item.getShortDesc() + ' has unsupported prerequisites.');
     }
   });
 };
