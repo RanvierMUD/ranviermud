c3e869d544e1512456f8eb4cc71b88cb8884f76d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-22 14:22:24 -0400

    Change filename to work properly.

--- scripts/objects/10-chain_whip.js
+++ scripts/objects/10-chain_whip.js
@@ -32,6 +32,7 @@ exports.listeners = {
       });
 
       if (!missedPrerequisites.length) {
+        player.warn('You could sow destruction with this.')
         const bonus = Math.round(player.getAttribute('stamina') / 2);
         player.combat.addDamageMod({
           name: 'chain_whip ' + this.getUuid(),
