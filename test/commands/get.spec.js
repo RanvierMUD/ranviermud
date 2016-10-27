686d54fb14c0365ee4bb8a2a1788e310d172dfad
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-27 17:27:42 -0400

    Add test cases for weight. Use warn in get cmd file.

--- test/commands/get.spec.js
+++ test/commands/get.spec.js
@@ -32,7 +32,11 @@ describe('successfully getting something from a room', () => {
 
 });
 
-describe('failing to get stuff from a room', () => {
+describe.only('failing to get stuff from a room', () => {
+  const location = 2;
+  const newRoom = new Room({ location });
+  rooms.addRoom(newRoom);
+  player.setLocation(location);
 
   it('should not let you get things during combat', () => {
     player.setInCombat({});
@@ -47,15 +51,41 @@ describe('failing to get stuff from a room', () => {
   });
 
   it('should not let you get an item that is not in your room', () => {
-    const location = 2;
-    const newRoom = new Room({ location });
-    rooms.addRoom(newRoom);
-    player.setLocation(location);
 
     get('potatos', player);
 
     const call = player.warn.getCall(1);
-    console.log(call.args);
     expect(call.args[0] === 'The potatos could not be found here.').to.be.true;
+  });
+
+  it('should not let you get an item if it is infinitely heavy...', () => {
+    const attributes = { weight: Infinity };
+    const keywords = ['burrito'];
+    const uuid = 'wat';
+    const heavyItemConfig = { name: 'burrito', keywords, attributes, location, uuid };
+    const heavyItem = new Item(heavyItemConfig);
+    items.addItem(heavyItem); //TODO: Make this consistent. Room shouldn't need the uuid passed, should just get it.
+    newRoom.addItem(heavyItem.getUuid());
+
+    get('burrito', player);
+
+    const call = player.warn.getCall(2);
+    expect(call.args[0] === "You are not able to carry that.").to.be.true;
+  });
+
+  it('should not let you get an item if it is too heavy...', () => {
+    const attributes = { weight: 200 };
+    const keywords = ['taco'];
+    const uuid = 'hokay';
+    const heavyItemConfig = { name: 'taco', keywords, attributes, location, uuid };
+    const heavyItem = new Item(heavyItemConfig);
+    items.addItem(heavyItem); //TODO: Make this consistent. Room shouldn't need the uuid passed, should just get it.
+    newRoom.addItem(heavyItem.getUuid());
+
+    get('taco', player);
+
+    const call = player.warn.getCall(3);
+    console.log(call.args);
+    expect(call.args[0] === "You are not able to carry that.").to.be.true;
   });
 });
