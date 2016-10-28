ba3e433171ab76d5692a4f4b4fa8ab9bd471a945
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-27 21:13:26 -0400

    Add failing test for getting an item.

--- test/commands/get.spec.js
+++ test/commands/get.spec.js
@@ -28,15 +28,12 @@ players.addPlayer(player);
 sinon.spy(player, 'warn');
 sinon.spy(items, 'get');
 
-describe('successfully getting something from a room', () => {
+const location = 2;
+const newRoom = new Room({ location });
+rooms.addRoom(newRoom);
+player.setLocation(location);
 
-});
-
-describe.only('failing to get stuff from a room', () => {
-  const location = 2;
-  const newRoom = new Room({ location });
-  rooms.addRoom(newRoom);
-  player.setLocation(location);
+describe('failing to get stuff from a room', () => {
 
   it('should not let you get things during combat', () => {
     player.setInCombat({});
@@ -55,6 +52,7 @@ describe.only('failing to get stuff from a room', () => {
     get('potatos', player);
 
     const call = player.warn.getCall(1);
+    expect(items.get.called).to.be.false;
     expect(call.args[0] === 'The potatos could not be found here.').to.be.true;
   });
 
@@ -66,10 +64,11 @@ describe.only('failing to get stuff from a room', () => {
     const heavyItem = new Item(heavyItemConfig);
     items.addItem(heavyItem); //TODO: Make this consistent. Room shouldn't need the uuid passed, should just get it.
     newRoom.addItem(heavyItem.getUuid());
-
     get('burrito', player);
 
     const call = player.warn.getCall(2);
+    console.log('infinite heavy:', call.args[0]);
+
     expect(call.args[0] === "You are not able to carry that.").to.be.true;
   });
 
@@ -85,7 +84,25 @@ describe.only('failing to get stuff from a room', () => {
     get('taco', player);
 
     const call = player.warn.getCall(3);
-    console.log(call.args);
+    console.log('very heavy:', call.args[0]);
     expect(call.args[0] === "You are not able to carry that.").to.be.true;
   });
+});
+
+describe('successfully getting something from a room', () => {
+
+  it('should be able to get a single item', () => {
+    const keywords = ['burger'];
+    const uuid = 'ham';
+    const burgerConfig = { name: 'burger', keywords, uuid, location }
+    const burger = new Item(burgerConfig);
+    items.addItem(burger);
+    newRoom.addItem(burger.getUuid());
+
+    get('burger', player);
+
+    expect(player.getInventory().includes(uuid)).to.be.true;
+
+  });
+
 });
