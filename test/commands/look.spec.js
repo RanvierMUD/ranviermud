c4b67e9c2d33e65af5699320f763335579097e87
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-11-06 18:56:33 -0500

    Set up test to look for room title, fix goofy errors until we get a failing test.

--- test/commands/look.spec.js
+++ test/commands/look.spec.js
@@ -6,8 +6,10 @@ const sinon  = require('sinon');
 const CommandInjector = require('./command-mock-utils').CommandInjector;
 const getGlobals      = require('./command-mock-utils').getGlobals;
 const addItem         = require('./command-mock-utils').addItem;
+const addNpc          = require('./command-mock-utils').addNpc;
 const getCallCounter  = require('./command-mock-utils').getCallCounter;
 
+const Time   = require('../../src/time').Time;
 const Player = require('../../src/player').Player;
 const Npc    = require('../../src/npcs').Npc;
 const Room   = require('../../src/rooms').Room;
@@ -29,6 +31,9 @@ const [ rooms, items, players, npcs, Commands ] = globals;
 players.addPlayer(player);
 
 sinon.spy(player, 'say');
+sinon.stub(Time, 'isDay');
+
+const getPlayerSayCall = getCallCounter(player.say);
 
 const location = 4;
 const description = 'room desc';
@@ -36,17 +41,20 @@ const short_description = 'room shortdesc';
 const area = 'The Dungeon';
 const title = 'Treasure Chamber';
 const room = new Room({ location, short_description, description });
+
 rooms.addRoom(room);
 player.setLocation(location);
 player.setPreference('roomdescs', 'default');
+
 const shield = addItem({
   items, room, location,
   short_description: 'a shield',
   keywords: ['shield'],
   uuid: 'shield'
 });
+
 const goblin = addNpc({
-  room, location, npcs
+  room, location, npcs,
   short_description: 'a goblin',
   keywords: ['goblin'],
   uuid: 'gobbo'
@@ -54,9 +62,14 @@ const goblin = addNpc({
 
 describe('Looking at a room', () => {
   it('should describe the room with a long description, the first time around', () => {
+    Time.isDay.returns(true);
+
     look('', player);
+
     // check for all player.say calls...
+    const titleCall = getPlayerSayCall();
     const expectedTitle = area + ': ' + title;
+    expect(titleCall.args[0] === expectedTitle).to.be.true;
   });
 });
 
