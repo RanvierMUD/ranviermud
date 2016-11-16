ed474f182f04b95e015d3ad903c65778d288a7b2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-11-16 12:33:11 -0500

    Added debug command for all items in the game. Found out that the chain whip had the same vnum as cleaver, which prevented the cleaver from spawning.

--- src/commands.js
+++ src/commands.js
@@ -119,6 +119,35 @@ const Commands = {
 
         },
 
+    debugItems: (rooms, items, players, npcs, Commands) =>
+      (player, args) => {
+        const allItems = items.objects;
+        player.warn("GLOBAL ITEMS:\n");
+        for (let uid in allItems) {
+          const item = allItems[uid];
+
+          player.say(item.getShortDesc());
+          player.say('\n')
+          player.say('vnum: ' + item.getVnum());
+          player.say('uuid: ' + item.getUuid());
+          player.say('location: ' + item.getRoom());
+          player.say('\n')
+          player.say(item.isEquipped() ? 'Equipped' : 'Not equipped');
+
+          const container = item.getContainer();
+          player.say(container ? 'Container: ' + container : 'No container.');
+
+          const inventory = item.getInventory();
+          player.say(inventory ? 'Inventory: ' : 'No inventory.');
+          if (inventory) {
+            for (let uid in inventory) {
+              player.say(uid);
+            }
+          }
+          player.warn('========\n');
+        }
+
+      },
 
     setAttribute: (rooms, items, players, npcs, Commands) =>
       (player, args) => {
