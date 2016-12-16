7d5c579e5accaf01744da4caf91a3887bbed844a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-16 17:40:54 -0500

    So much for TDD -- had some ideas about implementing checks for a timed effect ending.

--- src/effect.js
+++ src/effect.js
@@ -5,6 +5,8 @@ const _id      = Symbol('id');
 const _options = Symbol('options');
 const _type    = Symbol('type');
 const _target  = Symbol('target'); 
+const _elapsed = Symbol('elapsed');
+const _started = Symbol('started');
 
 /* Effect class -- instantiates new effect, can be used to check for validity of effect, can be used 
  * @param {id: string, options: object, type: string, target: NPC | Player } 
@@ -15,9 +17,19 @@ class Effect {
   constructor({ id, options, type, target }) {
     validate(id, options, type, target);
     this[_id]      = id;
-    this[_options] = options;
     this[_type]    = type;
     this[_target]  = target;
+    this[_options] = options;
+
+    //TODO: If the effect has a set duration, 
+    // then we need to keep track of how long it lasts
+    // so that players can not just log off to escape negative status effects.
+    // Do this by tracking
+    if (options.duration) {
+      this[_elapsed] = options.elapsed || 0;
+      this[_started] = Date.now();
+      target.on('quit', this.setElapsed);
+    }
   }
 
   /* Get private fields... */
@@ -27,8 +39,20 @@ class Effect {
   getTarget()  { return this[_target]; }
 
   /* Get options or defaults */
-  getDuration() { return parseInt(this[_options].duration, 10) || Infinity; }
+  getDuration()     { return parseInt(this[_options].duration, 10) || Infinity; }
+  getMultiplier()   { return parseInt(this[_options].factor, 10 )  || 1; }
+  getBonus()        { return parseInt(this[_options].bonus, 10)    || 0; }
 
+  /* Mutators */
+  setElapsed() { 
+    this[_elapsed] = Date.now() - this[_started];
+    return this[_elapsed];
+  }
+
+  /* Predicates */
+
+  isEnded() { return this.setElapsed() > this.getDuration(); }
+   
 }
 
 /* Validation helper for effect construction */
