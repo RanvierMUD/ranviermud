3282b94434746fc14143d2f4a169355cb72d3d17
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-16 12:01:00 -0500

    Test constructor for valid function. Use symbols as private field keys. Extract validation helper function.

--- src/effect.js
+++ src/effect.js
@@ -1,15 +1,43 @@
-const Type = require('./type');
+'use strict'
 
+/* Symbols for private fields of effect class */
+const _id      = Symbol('id');
+const _options = Symbol('options');
+const _type    = Symbol('type');
+const _target  = Symbol('target'); 
+
+/* Effect class -- instantiates new effect, can be used to check for validity of effect, can be used 
+ * @param {id: string, options: object, type: string, target: NPC | Player } 
+ * @return 
+*/
 class Effect {
-  constructor({id, options, type, target}) {
-    
-    // Validate.
-    if (!id) { throw new ReferenceError("Effects must have an ID to prevent stacking."); }
-    if (!options || !Object.keys(options).length) { throw new ReferenceError("Effects must take an options object to pass to applicable functions when evaluating effects."); }
-    if (!type) { throw new ReferenceError("Effects must have a generic effect type."); }
-    if (!target) { throw new ReferenceError("Effects must have a target."); }
-    if (!(Type.isPlayer(target) || Type.isNpc(target))) { throw new TypeError("Effects can only target players or NPCs."); }
 
+  constructor({ id, options, type, target }) {
+    validate(id, options, type, target);
+    this[_id]      = id;
+    this[_options] = options;
+    this[_type]    = type;
+    this[_target]  = target;
+  }
+
+}
+
+/* Validation helper for effect construction */
+const validate = (id, options, type, target) => {
+  if (!id) { 
+    throw new ReferenceError("Effects must have an ID to prevent stacking."); 
+  }
+  if (!options || !Object.keys(options).length) { 
+    throw new ReferenceError("Effects must take an options object to pass to applicable functions when evaluating effects."); 
+  }
+  if (!type) { 
+    throw new ReferenceError("Effects must have a generic effect type."); 
+  }
+  if (!target) { 
+    throw new ReferenceError("Effects must have a target."); 
+  }
+  if (!(target.addEffect)) { 
+    throw new TypeError("Effects can only target players or NPCs."); 
   }
 }
 
