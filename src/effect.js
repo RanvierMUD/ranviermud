436dd06a7108a6b6a391ae067b35658b8d670722
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-17 10:36:41 -0500

    Prefer using isNaN :cat:

--- src/effect.js
+++ src/effect.js
@@ -46,7 +46,7 @@ class Effect {
   setElapsed() { this[_elapsed] = this.getElapsed(); }
 
   getElapsed() { 
-    if (typeof this[_started] !== 'number') { return null; }
+    if (isNaN(this[_started])) { return null; }
 
     return Date.now() - this[_started];
   }
