4cdd94e609d9acc50524b56838f847ac0411d977
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-24 23:09:27 -0500

    Flesh out the stun effect. :neckbeard:

--- effects/stun.js
+++ effects/stun.js
@@ -4,10 +4,39 @@ const util = require('util');
 
 exports.effect = (players, items, npcs, rooms, Commands) => 
   (options, target) => {
-    util.log("LOL");
+    util.log("Stun attempt:");
     return {
-      activate: () => {},
-      deactivate: () => {},
+      activate: () => {
+        const {
+          factor          = 2,
+          duration        = 10 * 1000,
+        } = options;
+
+        const name = 'stunned';
+
+        target.combat.addSpeedMod({ name,
+          effect: speed => speed * factor
+        });
+
+        target.combat.addDodgeMod({ name,
+          effect: dodge => 0
+        });
+
+        target.combat.addToHitMod({
+          name,
+          effect: toHit => toHit / factor
+        });
+
+      },
+
+      modifiers: {
+        energy:     energy     => energy     / factor,
+        max_energy: max_energy => max_energy / factor,
+      },
+
+      deactivate: () => target.combat.removeAllMods('stunned'),
       type: 'stun',
+      name: 'Stunned',
+      aura: 'uselessness'
     };
   }
