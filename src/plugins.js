fd023283866c63b8474b18deecfeea4affe74289
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-01-03 16:47:42 -0500

    Cleanup by using camelCase and const. :cat:

--- src/plugins.js
+++ src/plugins.js
@@ -1,35 +1,35 @@
-var fs     = require('fs'),
-    util   = require('util');
-var plugins_dir = __dirname + '/../plugins/';
+const fs     = require('fs'),
+      util   = require('util');
+const pluginsDir = __dirname + '/../plugins/';
 
 module.exports = {
 	init(verbose, config) {
 		const log   = message => { if (verbose) util.log(message); };
 		const debug = message => { if (verbose) util.debug(message); };
 		
-		log("Examining plugin directory - " + plugins_dir);
-		if (!fs.existsSync(plugins_dir)) {
-			fs.mkdirSync(plugins_dir);
+		log("Examining plugin directory - " + pluginsDir);
+		if (!fs.existsSync(pluginsDir)) {
+			fs.mkdirSync(pluginsDir);
 		}
 
-		const plugins = fs.readdirSync(plugins_dir);
+		const plugins = fs.readdirSync(pluginsDir);
 		// Load any plugin files
 		plugins.forEach(plugin => {
-			const plugin_dir = plugins_dir + plugin;
-			if (!fs.statSync(plugin_dir).isDirectory()) return;
+			const pluginDir = pluginsDir + plugin;
+			if (!fs.statSync(pluginDir).isDirectory()) return;
 
 			// create and load the plugins
-			const files = fs.readdirSync(plugin_dir);
+			const files = fs.readdirSync(pluginDir);
 
 			// Check for an area manifest
-			const has_init = files.some(file => file.match(/plugin.js/));
+			const hasInit = files.some(file => file.match(/plugin.js/));
 
-			if (!has_init) {
+			if (!hasInit) {
 				return debug("Failed to load plugin - " + plugin + ' - No plugin.js file');
 			}
 
 			log("Loading plugin [" + plugin + "]");
-			require(plugin_dir + '/plugin.js').init(config);
+			require(pluginDir + '/plugin.js').init(config);
 			log("Done");
 		});
 	}
