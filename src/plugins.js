f5db36c2b13ef6b3042606e0654c294c7bb07816
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-01-03 16:34:33 -0500

    Make plugin init handle lack of plugins directory gracefully by creating an empty one rather than throwing an error.

--- src/plugins.js
+++ src/plugins.js
@@ -2,37 +2,35 @@ var fs     = require('fs'),
     util   = require('util');
 var plugins_dir = __dirname + '/../plugins/';
 
-module.exports =
-{
-	init: function (verbose, config)
-	{
-		var log = function (message) { if (verbose) util.log(message); };
-		var debug = function (message) { if (verbose) util.debug(message); };
+module.exports = {
+	init(verbose, config) {
+		const log   = message => { if (verbose) util.log(message); };
+		const debug = message => { if (verbose) util.debug(message); };
+		
 		log("Examining plugin directory - " + plugins_dir);
-		var plugins = fs.readdirSync(plugins_dir);
+		if (!fs.existsSync(plugins_dir)) {
+			fs.mkdirSync(plugins_dir);
+		}
+
+		const plugins = fs.readdirSync(plugins_dir);
 		// Load any plugin files
-		plugins.forEach(function (plugin)
-		{
-			var plugin_dir = plugins_dir + plugin;
+		plugins.forEach(plugin => {
+			const plugin_dir = plugins_dir + plugin;
 			if (!fs.statSync(plugin_dir).isDirectory()) return;
 
 			// create and load the plugins
-			var files = fs.readdirSync(plugin_dir);
+			const files = fs.readdirSync(plugin_dir);
 
 			// Check for an area manifest
-			var has_init = files.some(function (file) {
-				return file.match(/plugin.js/);
-			});
+			const has_init = files.some(file => file.match(/plugin.js/));
 
 			if (!has_init) {
-				log("Failed to load plugin - " + plugin + ' - No plugin.js file');
-				return;
+				return debug("Failed to load plugin - " + plugin + ' - No plugin.js file');
 			}
 
 			log("Loading plugin [" + plugin + "]");
 			require(plugin_dir + '/plugin.js').init(config);
 			log("Done");
 		});
-
 	}
 };
