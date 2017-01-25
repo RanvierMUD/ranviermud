45ffbe46897aa34ef40fa283586d792b20984b95
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-01-25 16:46:45 -0500

    Simplify API to just whitelist ONLY the local IP.

--- src/WebInterface.js
+++ src/WebInterface.js
@@ -1,10 +1,10 @@
 const util   = require('util');
 const fs     = require('fs');
-const jwt    = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 // Set up an Express app for the Web API.
 const express    = require('express');
+const whitelist  = require('whitelist-ips');
 const bodyParser = require('body-parser');
 const app        = express();
 const router     = express.Router();
@@ -17,36 +17,28 @@ class WebInterface {
   constructor(state) {
     this.state  = state;
     this.port   = Config.get('webPort') || 9000;
-    this.secretFile = __dirname + '/../.ranvierSecret';
+
+    this.whiteListed = [
+      'localhost'
+    ];
   }
 
   init() {
     util.log('[WEB] Initializing...');
-    this.generateSecret();
+    this.setUpMiddleWare();
     this.setUpGetRoutes();
-
-    // Authentication route.
-    router.post('/authenticate', (req, res) => this.authenticate(req, res));
-
-    app.use('/api', router);
-
     app.listen(this.port);
     util.log(`[WEB]: Web API activated and running on port ${this.port}.`);
     util.log(`[WEB]: Web API Secret: ${this.secret}`);
   }
 
-  generateSecret() {
-    try {
-      this.secret = fs.readFileSync(this.secretFile);
-    } catch(e) {
-      const salt  = bcrypt.genSaltSync(10);
-      this.secret = bcrypt.hashSync('ranvier', salt);
-      fs.writeFileSync(this.secretFile, this.secret);
-    }
-  }
-
-  getSecret() {
-    return this.secret;
+  setUpMiddleWare() {
+    app.use('/api', router);
+    app.use(whitelist(['127.0.0.1']));
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Server Error');
+    });
   }
 
   setUpGetRoutes() {
@@ -63,14 +55,7 @@ class WebInterface {
   getResponseData(manager, name) {
     return (req, res) => {
       const response = this.parseEntitiesIntoResponse(manager, name);
-      util.log(`[WEB] Request made for ${name}.`);
-
-      try {
-        return res.json({ [name]: response });
-      } catch (err) {
-        util.log(`[WEB] Request failed: ${err}`);
-        return res.json({ success: false, message: `An error has occurred: ${err}.` });
-      } 
+      return res.json({ success: false, message: `An error has occurred: ${err}.` });
     }
   }
 
@@ -95,28 +80,6 @@ class WebInterface {
     }
 
     return response;
-  }
-
-  authenticate(req, res) {
-    const accountName = req.body.name;
-    const account = this.state.AccountManager.findByName(accountName);
-    
-    if (!account) {
-      util.log(`[WEB] ${accountName} Auth Failed: Account not found.`);
-      return res.json({ success: false, message: `${accountName} was not found.`});
-    }
-
-    if (!bcrypt.compareSync(req.body.password, account.password)) {
-      util.log(`[WEB] ${accountName} Auth Failed: Incorrect password.`);
-      return res.json({ success: false, message: `${accountName} password incorrect.`});
-    }
-
-    let secret    = this.getSecret();
-    let expiresIn = "24h";
-    const token   = jwt.sign(account, secret, { expiresIn });
-
-    util.log(`[WEB] ${accountName} Auth Success!`);
-    return res.json({ success: true, token, message: `${accountName} authenticated. Token good for ${expiresIn}.`});
   }
 
 }
