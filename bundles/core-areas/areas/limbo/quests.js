46b887596e421314443170bc04c9e364204832f0
Shawn Biddle <shawncplus@gmail.com>
2017-02-04 14:50:01 -0500

    Fix quest typos

--- bundles/core-areas/areas/limbo/quests.js
+++ bundles/core-areas/areas/limbo/quests.js
@@ -10,7 +10,7 @@ module.exports = (srcPath) => {
       type: FetchQuest,
       config: {
         title: "Find A Weapon",
-        desc: "You're defenseless! Pick up the shiv in the next room.",
+        desc: "You're defenseless! Pick up the shiv on the ground by typing 'get shiv'",
         targetCount: 1,
         targetItem: "limbo:1",
         autoComplete: true,
@@ -22,7 +22,7 @@ module.exports = (srcPath) => {
       type: FetchQuest,
       config: {
         title: "One Cheese Please",
-        desc: "A rat has tasks you with finding it some cheese, better get to it.",
+        desc: "A rat has tasked you with finding it some cheese, better get to it.",
         targetCount: 1,
         targetItem: "limbo:2",
         removeItem: true,
