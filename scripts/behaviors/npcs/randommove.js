0c1e009635c0f6ce958ae5b4a23368298ce795fe
Shawn Biddle <shawncplus@gmail.com>
2017-02-04 20:26:59 -0500

    Remove unused scripts dir

--- scripts/behaviors/npcs/randommove.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const Random = require('../../../src/random.js').Random;
-const chooseRandomExit = require('../../../src/pathfinding.js').chooseRandomExit;
-
-const failureRoll = 5;
-
-exports.listeners = {
-  tick: chooseRandomExit(failureRoll)
-};
