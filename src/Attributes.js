63d77c2ff98b677194aeed0a329ec8eeae98afc9
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-02-08 10:57:27 -0500

    Remove a lot of unused methods from Attributes class.

--- src/Attributes.js
+++ src/Attributes.js
@@ -8,7 +8,6 @@ class Attributes extends Map
 
     // Rehydrate or create new attributes.
     for (let [name, value] of Object.entries(attributes)) {
-      console.log("name:", name, "value:", value);
       if (isNaN(value)) {
         const { base, delta } = value;
         this.set(name, new Attribute(name, base, delta));
@@ -22,32 +21,10 @@ class Attributes extends Map
     return this.entries();
   }
 
-  /* Attributes are calculated by the maximum value subtracted from 
-   * @param 
-   * @return 
-  */
-  getAttribute(attr) {
-    const delta = this.getDeltaKey(attr);
-    return this.get(attr) + this.get(deltaKey);
-  }
-
-  incrementAttribute(attr, value) {
-    const deltaKey = this.getDeltaKey(attr);
-    const currentDelta = this.get(deltaKey);
-    const newDelta = Math.max(0, currentDelta - value);
-    this.set(deltaKey, newDelta);
-  }
-
-  decrementAttribute(attr, value) {
-    this.incrementAttribute(attr, -value);
-  }
-
-  getDeltaKey(attr) {
-    return `${attr}Delta`;
-  }
-
-  setBaseAttribute(attr, value) {
-
+  clearDeltas() {
+    for (let [_, attr] of this) {
+      attr.setDelta(0);
+    }
   }
 
 }
