e8ccb71fa5bf6eaeb5cdce52ac91fb3859dddf45
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-02-20 12:52:53 -0500

    Use HelpManager.search to find possible results, and display them... or not.

--- bundles/core-commands/commands/help.js
+++ bundles/core-commands/commands/help.js
@@ -6,12 +6,40 @@ module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
 
   return {
-    usage: 'help',
+    usage: 'help [search] [topic keyword]',
     command: (state) => (args, player) => {
       if (!args.length) {
         // look at `help help` if they haven't specified a file
         return state.CommandManager.get('help').execute('help', player);
       }
+
+      // `help search`
+      if (args.indexOf('search') === 0) {
+        args = args.split(' ').slice(1).join(' ');
+        if (!args.length) {
+          // `help search` syntax is included in `help help`
+          return state.CommandManager.get('help').execute('help', player);
+        }
+
+        const results = state.HelpManager.find(args);
+        if (!results.size) {
+          return Broadcast.sayAt(player, "Sorry, no results were found for your search.");
+        }
+        if (results.size === 1) {
+          const [ _, hfile ] = [...results][0];
+          return Broadcast.sayAt(player, hfile.render(state));
+        }
+
+        Broadast.sayAt(player, "<white>Search Results:</white>");
+        Broadcast.sayAt(player, "-----------------------------------------");
+
+        for (const [name, help] of results) {
+          Broadcast.sayAt(player, `<cyan>${name}</cyan>`);
+        }
+        return;
+      }
+
+
       const hfile = state.HelpManager.get(args);
 
       if (!hfile) {
