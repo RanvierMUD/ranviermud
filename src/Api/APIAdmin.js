bd08b67cab18ccf169af5f95b7945038c218faf4
Josh Williams <williams.jackj@gmail.com>
2017-02-24 14:19:38 -0600

    WIP Add back in Sean's original endpoints, but move them to /api/admin since they are reflecting "live game data".

--- src/Api/APIAdmin.js
+++ src/Api/APIAdmin.js
@@ -12,6 +12,13 @@ class APIAdmin {
   }
 
   setupRoutes() {
+    const { MobFactory, PlayerManager, ItemManager, RoomManager, HelpManager } = this.state;
+
+    router.get('/npcs', this.getResponseData(MobFactory, 'entities'));
+    router.get('/players', this.getResponseData(PlayerManager, 'players'));
+    router.get('/items', this.getResponseData(ItemManager, 'items'));
+    router.get('/rooms', this.getResponseData(RoomManager, 'rooms'));
+    router.get('/help', this.getResponseData(HelpManager, 'helps'));
 
     router.get('/config', this.getConfig());
     router.put('/config', celebrate({body: validators.config}), this.putConfig());
@@ -31,6 +38,35 @@ class APIAdmin {
       const response = Config.getAll();
       return res.json(response);
     };
+  }
+
+  getResponseData(manager, name) {
+    return (req, res) => {
+      const response = this.parseEntitiesIntoResponse(manager, name);
+      return res.json({ [name]: response });
+    };
+  }
+
+  parseEntitiesIntoResponse(manager, name) {
+    const entities = manager[name];
+    let response = [];
+
+    if (entities instanceof Set) {
+      response = [...entities];
+    }
+
+    if (entities instanceof Map) {
+      handleMap(entities, response);
+    }
+
+    function handleMap(entities, response) {
+      for (let [key, value] of entities) {
+        if (!value) { continue; }
+        const val = value.serialize ? value.serialize() : value;
+        response.push(val);
+      }
+    }
+    return response;
   }
 }
 
