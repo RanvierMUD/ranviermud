563c867907a070785076ebb5e8e7930136f8e032
Josh Williams <williams.jackj@gmail.com>
2017-02-23 23:27:07 -0600

    WIP Remove old routes

--- src/Api/APIBuilder.js
+++ src/Api/APIBuilder.js
@@ -34,12 +34,6 @@ class APIBuilder {
     router.put('/bundles/:bundleName/areas/:areaName/items', celebrate({body: validators.item}), this.putItem.bind(this));
     router.put('/bundles/:bundleName/areas/:areaName/rooms', celebrate({body: validators.room}), this.putRoom.bind(this));
 
-    router.get('/npcs', this.getResponseData(MobFactory, 'entities'));
-    router.get('/players', this.getResponseData(PlayerManager, 'players'));
-    router.get('/items', this.getResponseData(ItemManager, 'items'));
-    router.get('/rooms', this.getResponseData(RoomManager, 'rooms'));
-    router.get('/help', this.getResponseData(HelpManager, 'helps'));
-
     return router;
   }
 
@@ -168,35 +162,6 @@ class APIBuilder {
     } catch (err){
       res.status(400).send(err.message);
     }
-  }
-
-  getResponseData(manager, name) {
-    return (req, res) => {
-      const response = this.parseEntitiesIntoResponse(manager, name);
-      return res.json({ [name]: response });
-    };
-  }
-
-  parseEntitiesIntoResponse(manager, name) {
-    const entities = manager[name];
-    let response = [];
-
-    if (entities instanceof Set) {
-      response = [...entities];
-    }
-
-    if (entities instanceof Map) {
-      handleMap(entities, response);
-    }
-
-    function handleMap(entities, response) {
-      for (let [key, value] of entities) {
-        if (!value) { continue; }
-        const val = value.serialize ? value.serialize() : value;
-        response.push(val);
-      }
-    }
-    return response;
   }
 }
 
