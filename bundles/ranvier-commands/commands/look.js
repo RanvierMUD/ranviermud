d3e8f6ccb334d6e2df1aa9354a7f642d72847f88
Shawn Biddle <shawncplus@gmail.com>
2017-03-04 14:46:44 -0500

    Show exits in addition to having a compass

--- bundles/ranvier-commands/commands/look.js
+++ bundles/ranvier-commands/commands/look.js
@@ -105,6 +105,10 @@ module.exports = (srcPath) => {
       }
       Broadcast.sayAt(player, '[NPC] ' + npc.name);
     });
+
+    Broadcast.at(player, '[<yellow><bold>Exits</yellow></bold>: ');
+      Broadcast.at(player, Array.from(room.exits).map(ex => ex.direction).join(' '));
+      Broadcast.sayAt(player, ']');
   }
 
   function lookEntity(player, args) {
