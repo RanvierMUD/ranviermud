74753006bd3fc2ddea75b1c814ad9de419e710ef
Josh Williams <williams.jackj@gmail.com>
2017-03-04 13:46:16 -0600

    Playing with some formatting and printing a directions map (#162)

--- bundles/ranvier-commands/commands/look.js
+++ bundles/ranvier-commands/commands/look.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const leftPad = require('left-pad');
+
 module.exports = (srcPath) => {
   const Broadcast = require(srcPath + 'Broadcast');
   const CommandParser = require(srcPath + 'CommandParser').CommandParser;
@@ -7,14 +9,63 @@ module.exports = (srcPath) => {
   const ItemType = require(srcPath + 'ItemType');
   const Logger = require(srcPath + 'Logger');
 
+  function getCompass(player) {
+    const room = player.room;
+
+    const exitMap = new Map();
+    exitMap.set('east', 'E');
+    exitMap.set('west', 'W');
+    exitMap.set('south', 'S');
+    exitMap.set('north', 'N');
+    exitMap.set('southwest', 'SW');
+    exitMap.set('southeast', 'SE');
+    exitMap.set('northwest', 'NW');
+    exitMap.set('northeast', 'NE');
+
+    const directionsAvailable = room.exits.map(exit => exitMap.get(exit.direction));
+
+    const exits = Array.from(exitMap.values()).map(exit => {
+      if (directionsAvailable.includes(exit)) {
+        return exit;
+      }
+      //If we are either SE or NE, pre-pad
+      if (exit.length === 2 && exit.includes('E')) {
+        return ' -';
+      }
+
+      //If we are either SW or NW, post-pad
+      if (exit.length === 2 && exit.includes('W')) {
+        return '- ';
+      }
+      return '-';
+    });
+
+    const [E, W, S, N, SW, SE, NW, NE] = exits;
+
+    const line1 = `<yellow><bold>${NW}</bold></yellow>     <yellow><bold>${N}</bold></yellow>     <yellow><bold>${NE}</bold></yellow>`;
+    const line2 = `<yellow><bold>${W}</bold></yellow> <---(M)---> <yellow><bold>${E}</bold></yellow>`;
+    const line3 = `<yellow><bold>${SW}</bold></yellow>     <yellow><bold>${S}</bold></yellow>     <yellow><bold>${SE}</bold></yellow>\r\n`;
+
+    return [line1, line2, line3];
+  }
+
   function lookRoom(state, player) {
     const room = player.room;
 
+    const [ line1, line2, line3 ] = getCompass(player);
+
     // Render the room
-    Broadcast.sayAt(player, room.title);
+
+    //The top line has 99 characters of ANSI coloring (114(COLORS) - 2(NW) - 1(N) - 2(NE) - 5(SPACING) - 5(SPACING))
+    //The 164 accounts for this
+    Broadcast.sayAt(player, `<yellow><bold>${room.title}</bold></yellow>` + leftPad(line1, 164 - room.title.length));
+    Broadcast.sayAt(player, '--------------------------------------------' + leftPad(line2, 90));
+    Broadcast.sayAt(player, leftPad(line3, 166));
+
     if (!player.getMeta('config.brief')) {
       Broadcast.sayAt(player, room.description, 80);
     }
+
     Broadcast.sayAt(player, '');
 
     // show all players
@@ -54,10 +105,6 @@ module.exports = (srcPath) => {
       }
       Broadcast.sayAt(player, '[NPC] ' + npc.name);
     });
-
-    Broadcast.at(player, '[<yellow><bold>Exits</yellow></bold>: ');
-      Broadcast.at(player, Array.from(room.exits).map(ex => ex.direction).join(' '));
-      Broadcast.sayAt(player, ']');
   }
 
   function lookEntity(player, args) {
