c24586c32cd068e8ff2b6f053f4507ebdda629bd
Shawn Biddle <shawncplus@gmail.com>
2017-03-19 19:08:58 -0400

    Small refactor of resource rendering into Crafting class

--- bundles/ranvier-crafting/commands/resources.js
+++ bundles/ranvier-crafting/commands/resources.js
@@ -4,10 +4,7 @@
 
 module.exports = (srcPath, bundlePath) => {
   const B = require(srcPath + 'Broadcast');
-  const Data =require(srcPath + 'Data');
-  const Item = require(srcPath + 'Item');
-
-  const dataPath = bundlePath + 'ranvier-crafting/data/';
+  const Crafting = require(bundlePath + 'ranvier-crafting/lib/Crafting');
 
   return {
     aliases: [ "materials" ],
@@ -18,24 +15,14 @@ module.exports = (srcPath, bundlePath) => {
         return B.sayAt(player, "You haven't gathered any resources.");
       }
 
-      const resources = Data.parseFile(dataPath + 'resources.yml');
-
       B.sayAt(player, '<b>Resources</b>');
       B.sayAt(player, B.line(40));
       let totalAmount = 0;
       for (const resourceKey in playerResources) {
         const amount = playerResources[resourceKey];
-        const resourceDef = resources[resourceKey];
         totalAmount += amount;
 
-        // create a temporary fake item for the resource for rendering purposes
-        const resItem = new Item(null, {
-          name: resourceDef.title,
-          quality: resourceDef.quality,
-          keywords: resourceKey,
-          id: 1
-        });
-
+        const resItem = Crafting.getResourceItem(resourceKey);
         B.sayAt(player, `${resItem.display} x ${amount}`);
       }
 
