1147bd56dc60672b64c8fe2a0ef2aec773ba3a3d
Mike Du Russel <mdurussel@squarespace.com>
2017-03-24 21:48:40 -0400

    Add a teleport command to the debug-commands bundle

---
+++ bundles/debug-commands/commands/teleport.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+  const PlayerRoles = require(srcPath + 'PlayerRoles');
+
+  return {
+    aliases: ['tp'],
+    usage: 'teleport <player/room>',
+    requiredRole: PlayerRoles.ADMIN,
+    command: (state) => (args, player) => {
+      if (!args || !args.length) {
+        return Broadcast.sayAt(player, 'Must specify a destination using an online player or room entity reference.');
+      }
+
+      const target = args;
+      const isPlayer = target.indexOf(':') === -1 ? true : false;
+      let targetRoom = null;
+
+      if (isPlayer) {
+        const targetPlayer = state.PlayerManager.getPlayer(target);
+        if (!targetPlayer) {
+          return Broadcast.sayAt(player, 'No such player online.');
+        } else if (targetPlayer === player || targetPlayer.room === player.room) {
+          return Broadcast.sayAt(player, 'You try really hard to teleport before realizing you\'re already at your destination.');
+        }
+
+        targetRoom = targetPlayer.room;
+      } else {
+        targetRoom = state.RoomManager.getRoom(target);
+        if (!targetRoom) {
+          return Broadcast.sayAt(player, 'No such room entity reference exists.');
+        } else if (targetRoom === player.room) {
+          return Broadcast.sayAt(player, 'You try really hard to teleport before realizing you\'re already at your destination.');
+        }
+      }
+
+      const oldRoom = player.room;
+
+      player.moveTo(targetRoom, () => {
+        Broadcast.sayAt(player, '<b><green>You snap your finger and instantly appear in a new room.</green></b>');
+        Broadcast.sayAt(player, '');
+        state.CommandManager.get('look').execute('', player);
+      });
+
+      Broadcast.sayAt(oldRoom, `${player.name} teleports away.`);
+      Broadcast.sayAtExcept(targetRoom, `${player.name} teleports here.`, player);
+    }
+  }
+};
