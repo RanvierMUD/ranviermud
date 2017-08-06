4fa99be517d264545ec7ccdf85e05a039a3480da
Daniel <daniel.l.allen@gmail.com>
2017-08-05 23:59:09 -0400

    Fix craft list display. (#253)
    * Fix craft list display.
    
    * Variable name changed.

--- bundles/ranvier-crafting/commands/craft.js
+++ bundles/ranvier-crafting/commands/craft.js
@@ -45,8 +45,8 @@ module.exports = (srcPath, bundlePath) => {
           return say(player, B.center(40, "No recipes."));
         }
 
-        return category.items.forEach((item, index) => {
-          say(player, sprintf('%2d) ', index + 1) + item.display);
+        return category.items.forEach((categoryEntry, index) => {
+          say(player, sprintf('%2d) ', index + 1) + categoryEntry.item.display);
         });
       }
 
