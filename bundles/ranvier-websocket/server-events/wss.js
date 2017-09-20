d3b3a6113b3595bd3a28ced2464543f7284eef16
Shawn Biddle <shawncplus@gmail.com>
2017-09-19 23:45:19 -0400

    Enable the websocket bundle by default, fix bug with combat prompt when using both telnet and websocket

--- bundles/ranvier-websocket/server-events/wss.js
+++ bundles/ranvier-websocket/server-events/wss.js
@@ -13,7 +13,7 @@ module.exports = srcPath => {
     listeners: {
       startup: state => function (commander) {
         // create a new websocket server using the port command line argument
-        const wss = new WebSocket.Server({ port: commander.port });
+        const wss = new WebSocket.Server({ port: 4001 });
 
         // This creates a super basic "echo" websocket server
         wss.on('connection', function connection(ws) {
