727415fc2e7fbff1e897f4bea47ccb6fa3b83377
Shawn Biddle <shawncplus@gmail.com>
2017-09-19 22:34:53 -0400

    Fix windows bundle install staying in bundle dir after install

--- win-bundle-install.bat
+++ win-bundle-install.bat
@@ -1,8 +1,9 @@
 @echo off
 for /D %%i in (bundles/*) do (
   if exist "%cd%\bundles\%%i\package.json" (
-    cd "%cd%\bundles\%%i"
+    pushd "%cd%\bundles\%%i"
     echo Updating %%i
     call npm install >nul 2>&1
+    popd
   )
 )
