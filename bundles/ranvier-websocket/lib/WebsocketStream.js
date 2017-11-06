243b70f29fb7c5711c5058590db568deaf6e07ec
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-11-06 16:27:43 -0500

    Document use of the WS audio command.

--- bundles/ranvier-websocket/lib/WebsocketStream.js
+++ bundles/ranvier-websocket/lib/WebsocketStream.js
@@ -56,6 +56,13 @@ class WebsocketStream extends TransportStream
     }));
   }
 
+  /**
+   * To cue audio or music with Neuro.
+   * @param {String} audioCue - Should be the filename with extension, or without extension if the file is .mp3
+   * @param {Object} options
+   * @param {Boolean} options.looping - For looping music or ambient sound. Only one sound may loop at a time. Looping audio is also background audio by default.
+   * @param {Boolean} options.background - For music, ambient sound, or other long-playing bits of audio. May or may not loop.
+   */
   executeSendAudio(audioCue, options = {}) {
     if (!this.writable) {
       return;
