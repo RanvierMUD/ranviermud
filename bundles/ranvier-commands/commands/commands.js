34b72539d90938aba79f901d550e47e2a5da6124
Colin Kierans <vindexus@gmail.com>
2018-04-14 11:01:31 -0700

    `commands` list sorted alphabetically (#334)
    * 'commands' displays them in alphabetical order
    
    * Made the order go by column then row
    
    * Made a function for saying in columns. Implemented it in channel commands list

--- bundles/ranvier-commands/commands/commands.js
+++ bundles/ranvier-commands/commands/commands.js
@@ -3,8 +3,56 @@
 const sprintf = require('sprintf-js').sprintf;
 
 module.exports = (srcPath) => {
-  
   const Broadcast = require(srcPath + 'Broadcast');
+
+  function sayAtColumns (source, strings, numCols) {
+    //Build a 2D map of strings by col/row
+    let col = 0;
+    const perCol = Math.ceil(strings.length / numCols);
+    let rowCount = 0;
+    const colWidth = Math.floor((3 * 20) / numCols);
+    const columnedStrings = strings.reduce((map, string, index) => {
+      if (rowCount >= perCol) {
+        rowCount = 0;
+        col++;
+      }
+      map[col] = map[col] || [];
+
+      if (!map[col]) {
+        map.push([]);
+      }
+
+      map[col].push(string);
+      rowCount++;
+      return map;
+    }, [])
+
+    col = 0;
+    let row = 0;
+    let i = 0;
+    const said = [];
+    while(said.length < strings.length) {
+      if (columnedStrings[col] && columnedStrings[col][row]) {
+        const string = columnedStrings[col][row];
+        said.push(string);
+        Broadcast.at(source, sprintf("%-" + colWidth + "s", string));
+      }
+      i++;
+
+
+      col++;
+      if (col == numCols) {
+        Broadcast.sayAt(source);
+        col = 0;
+        row++;
+      }
+    }
+
+    // append another line if need be
+    if ((col) % numCols !== 0) {
+      Broadcast.sayAt(source);
+    }
+  }
 
   return {
     aliases: ['channels'],
@@ -14,35 +62,31 @@ module.exports = (srcPath) => {
       Broadcast.sayAt(player, "<bold><white>                  Commands</bold></white>");
       Broadcast.sayAt(player, "<bold><white>===============================================</bold></white>");
 
-      let i = 0;
+      let commands = [];
       for (let [ name, command ] of state.CommandManager.commands) {
-        if (player.role < command.requiredRole) {
-          continue;
+        if (player.role >= command.requiredRole) {
+          commands.push(name);
         }
+      }
 
-        Broadcast.at(player, sprintf("%-20s", name));
-        if (++i % 3 === 0) {
-          Broadcast.sayAt(player);
-        }
-      }
-      // append another line if need be
-      if ((i - 1) % 3 !== 0) {
-        Broadcast.sayAt(player);
-      }
+      commands.sort()
+      sayAtColumns(player, commands, 4)
 
       // channels
       Broadcast.sayAt(player);
       Broadcast.sayAt(player, "<bold><white>                  Channels</bold></white>");
       Broadcast.sayAt(player, "<bold><white>===============================================</bold></white>");
 
-      i = 0;
+      let i = 0;
+      let channelCommands = [];
       for (let [ name ] of state.ChannelManager.channels) {
-        Broadcast.at(player, sprintf("%-20s", name));
-        if (++i % 3 === 0) {
-          Broadcast.sayAt(player, '');
-        }
+          channelCommands.push(name);
       }
-      
+
+      channelCommands.sort();
+      sayAtColumns(player, channelCommands, 4)
+
+
       // end with a line break
       Broadcast.sayAt(player, '');
     }
